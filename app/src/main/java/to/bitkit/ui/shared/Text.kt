c5e60e691e0ac0fced0e292da218e77a05e1269a
Ovi Trif <ovitrif@proton.me>
2024-10-17 16:09:52 +0200

    feat: format numeric values

--- app/src/main/java/to/bitkit/ui/shared/Text.kt
+++ app/src/main/java/to/bitkit/ui/shared/Text.kt
@@ -5,15 +5,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.withStyle
 import to.bitkit.R
+import java.text.NumberFormat
 
 @Composable
 internal fun moneyString(
     value: String,
     currency: String = stringResource(R.string.sat),
 ) = buildAnnotatedString {
-    append("$value ")
+    append(NumberFormat.getNumberInstance(Locale.current.platformLocale).format(value.toLong()))
+    append(" ")
     withStyle(SpanStyle(color = colorScheme.onBackground.copy(0.5f))) {
         append(currency)
     }
