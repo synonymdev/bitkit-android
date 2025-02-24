7c1c626bf9c6a951746cfad528c325dba1ccd083
Ovi Trif <ovitrif@proton.me>
2025-02-24 17:48:08 +0100

    feat: Show no funds alert on Funding screen for 0 sats

--- app/src/main/java/to/bitkit/ui/screens/transfer/FundingScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/transfer/FundingScreen.kt
@@ -1,6 +1,8 @@
 package to.bitkit.ui.screens.transfer
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,10 +11,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -24,6 +32,7 @@ import to.bitkit.env.Env
 import to.bitkit.ui.LocalBalances
 import to.bitkit.ui.appViewModel
 import to.bitkit.ui.components.BodyM
+import to.bitkit.ui.components.BodyMB
 import to.bitkit.ui.components.Display
 import to.bitkit.ui.components.RectangleButton
 import to.bitkit.ui.scaffold.AppTopBar
@@ -40,6 +49,13 @@ fun FundingScreen(
     onBackClick: () -> Unit = {},
     onCloseClick: () -> Unit = {},
 ) {
+    val balances = LocalBalances.current
+    val canTransfer = remember(balances.totalOnchainSats) {
+        balances.totalOnchainSats >= Env.TransactionDefaults.recommendedBaseFee
+    }
+    var showNoFundsAlert by remember { mutableStateOf(false) }
+    val isGeoBlocked = appViewModel?.isGeoBlocked == true
+
     ScreenColumn {
         AppTopBar(
             titleText = stringResource(R.string.lightning__funding__nav_title),
@@ -62,13 +78,6 @@ fun FundingScreen(
             Display(text = stringResource(R.string.lightning__funding__title).withAccent(accentColor = Colors.Purple))
             Spacer(modifier = Modifier.height(8.dp))
 
-            val balances = LocalBalances.current
-            val canTransfer = remember(balances.totalOnchainSats) {
-                balances.totalOnchainSats >= Env.TransactionDefaults.recommendedBaseFee
-            }
-
-            val isGeoBlocked = appViewModel?.isGeoBlocked == true
-
             val text = if (isGeoBlocked) {
                 stringResource(R.string.lightning__funding__text_blocked)
             } else {
@@ -81,19 +90,35 @@ fun FundingScreen(
             Column(
                 verticalArrangement = Arrangement.spacedBy(8.dp),
             ) {
-                RectangleButton(
-                    label = stringResource(R.string.lightning__funding__button1),
-                    icon = {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_transfer),
-                            contentDescription = null,
-                            tint = Colors.Purple,
-                            modifier = Modifier.size(28.dp),
+                Box {
+                    RectangleButton(
+                        label = stringResource(R.string.lightning__funding__button1),
+                        icon = {
+                            Icon(
+                                painter = painterResource(R.drawable.ic_transfer),
+                                contentDescription = null,
+                                tint = Colors.Purple,
+                                modifier = Modifier.size(28.dp),
+                            )
+                        },
+                        enabled = canTransfer && !isGeoBlocked,
+                        onClick = onTransfer,
+                    )
+                    if (balances.totalOnchainSats == 0uL) {
+                        Box(
+                            modifier = Modifier
+                                .matchParentSize()
+                                .clickable(
+                                    enabled = balances.totalOnchainSats == 0uL,
+                                    interactionSource = null,
+                                    indication = null,
+                                    onClick = {
+                                        showNoFundsAlert = true
+                                    }
+                                )
                         )
-                    },
-                    enabled = canTransfer && !isGeoBlocked,
-                    onClick = onTransfer,
-                )
+                    }
+                }
                 RectangleButton(
                     label = stringResource(R.string.lightning__funding__button2),
                     icon = {
@@ -120,6 +145,23 @@ fun FundingScreen(
                     onClick = onAdvanced,
                 )
             }
+        }
+        if (showNoFundsAlert) {
+            AlertDialog(
+                onDismissRequest = { showNoFundsAlert = false }, // Dismiss the alert
+                confirmButton = {
+                    TextButton(onClick = { showNoFundsAlert = false }) {
+                        BodyM(text = stringResource(R.string.common__ok), color = Colors.Purple)
+                    }
+                },
+                title = {
+                    BodyMB(text = stringResource(R.string.lightning__no_funds__title))
+                },
+                text = {
+                    BodyM(text = stringResource(R.string.lightning__no_funds__description))
+                },
+                shape = MaterialTheme.shapes.small,
+            )
         }
     }
 }
