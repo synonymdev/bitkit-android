ff7c01fb71ec0e1d2e07ff8f4df38c1007a8e8d9
Joao Victor Sena <jvsena16@gmail.com>
2025-03-11 06:57:18 -0300

    fix: add vertical scroll

--- app/src/main/java/to/bitkit/ui/onboarding/CreateWalletScreen.kt
+++ app/src/main/java/to/bitkit/ui/onboarding/CreateWalletScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,6 +55,7 @@ fun CreateWalletScreen(
                 .fillMaxWidth()
                 .fillMaxHeight(.325f)
                 .align(Alignment.BottomCenter)
+                .verticalScroll(rememberScrollState())
         ) {
             Display(text = stringResource(R.string.onboarding__slide4_header).withAccent())
             Spacer(modifier = Modifier.height(8.dp))
