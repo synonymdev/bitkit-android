4bc40b4da7af22cf9a3620fedf1a5d38857be96b
Joao Victor Sena <jvsena16@gmail.com>
2025-03-11 08:50:32 -0300

    refactor: remove imports

--- app/src/main/java/to/bitkit/ui/onboarding/CreateWalletScreen.kt
+++ app/src/main/java/to/bitkit/ui/onboarding/CreateWalletScreen.kt
@@ -2,20 +2,16 @@ package to.bitkit.ui.onboarding
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
