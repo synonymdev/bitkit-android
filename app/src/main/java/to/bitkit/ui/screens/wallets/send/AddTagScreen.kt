cf139db80b2fe8b6c979a7d2caf6be1458825a43
Joao Victor Sena <jvsena16@gmail.com>
2025-03-17 09:44:13 -0300

    feat: input text

--- app/src/main/java/to/bitkit/ui/screens/wallets/send/AddTagScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/send/AddTagScreen.kt
@@ -9,9 +9,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import to.bitkit.R
@@ -19,8 +30,10 @@ import to.bitkit.ui.components.ButtonSize
 import to.bitkit.ui.components.Caption13Up
 import to.bitkit.ui.components.PrimaryButton
 import to.bitkit.ui.scaffold.SheetTopBar
+import to.bitkit.ui.theme.AppTextFieldDefaults
 import to.bitkit.ui.theme.AppThemeSurface
 import to.bitkit.ui.theme.Colors
+import to.bitkit.viewmodels.SendEvent
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
@@ -33,6 +46,9 @@ fun AddTagScreen(
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
+        var inputText by remember { mutableStateOf("") }
+
+
         SheetTopBar(stringResource(R.string.wallet__tags_add)) {
             onBack()
         }
@@ -50,7 +66,7 @@ fun AddTagScreen(
             FlowRow(
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.padding(bottom = 16.dp)
+                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
             ) {
                 tags.map { tagText ->
                     PrimaryButton( //TODO REPLACE WITH THE RIGHT COMPONENT
@@ -63,7 +79,23 @@ fun AddTagScreen(
             }
             Spacer(modifier = Modifier.height(16.dp))
             Caption13Up(text = stringResource(R.string.wallet__tags_new), color = Colors.White64)
-
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                placeholder = { Text(stringResource(R.string.wallet__tags_new_enter)) },
+                value = inputText,
+                onValueChange = { newText -> inputText = newText },
+                maxLines = 1,
+                singleLine = true,
+                colors = AppTextFieldDefaults.noIndicatorColors,
+                shape = MaterialTheme.shapes.small,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    onTagAdded(inputText)
+                }),
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
