f5858e39e466a9cf4a956721d77a1c9953d1e3c3
Joao Victor Sena <jvsena16@gmail.com>
2025-03-19 09:45:14 -0300

    fix: distinct items

--- app/src/main/java/to/bitkit/viewmodels/AppViewModel.kt
+++ app/src/main/java/to/bitkit/viewmodels/AppViewModel.kt
@@ -90,7 +90,7 @@ class AppViewModel @Inject constructor(
     fun addTagToSelected(newTag: String) {
         _sendUiState.update {
             it.copy(
-                selectedTags = it.selectedTags + newTag
+                selectedTags = (it.selectedTags + newTag).distinct()
             )
         }
     }
