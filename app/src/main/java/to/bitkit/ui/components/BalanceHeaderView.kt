3310e8272f6810826d723f3e82b532834aa8d3df
Joao Victor Sena <jvsena16@gmail.com>
2025-03-25 09:16:37 -0300

    refactor: extract code to the BalanceHeader component

--- app/src/main/java/to/bitkit/ui/components/BalanceHeaderView.kt
+++ app/src/main/java/to/bitkit/ui/components/BalanceHeaderView.kt
@@ -3,18 +3,23 @@ package to.bitkit.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import to.bitkit.models.ConvertedAmount
 import to.bitkit.models.PrimaryDisplay
 import to.bitkit.ui.LocalCurrencies
 import to.bitkit.ui.currencyViewModel
 import to.bitkit.ui.shared.util.clickableAlpha
+import to.bitkit.ui.theme.AppThemeSurface
+import to.bitkit.ui.theme.Colors
 
 @Composable
 fun BalanceHeaderView(
@@ -27,44 +32,64 @@ fun BalanceHeaderView(
     val (rates, _, _, _, displayUnit, primaryDisplay) = LocalCurrencies.current
     val converted: ConvertedAmount? = if (rates.isNotEmpty()) currency.convert(sats = sats) else null
 
-    Column(
-        verticalArrangement = Arrangement.spacedBy(4.dp),
-        horizontalAlignment = Alignment.Start,
-        modifier = modifier
-            .clickableAlpha { currency.togglePrimaryDisplay() }
-    ) {
-        converted?.let { converted ->
-            if (primaryDisplay == PrimaryDisplay.BITCOIN) {
-                Column {
-                    SmallRow(
-                        prefix = prefix,
-                        text = "${converted.symbol} ${converted.formatted}"
-                    )
+    converted?.let { converted ->
+        val btcComponents = converted.bitcoinDisplay(displayUnit)
 
-                    val btcComponents = converted.bitcoinDisplay(displayUnit)
-                    LargeRow(
-                        prefix = prefix,
-                        text = btcComponents.value,
-                        symbol = btcComponents.symbol,
-                        showSymbol = showBitcoinSymbol
-                    )
-                }
-            } else {
-                Column {
-                    val btcComponents = converted.bitcoinDisplay(displayUnit)
-                    SmallRow(
-                        prefix = prefix,
-                        text = "${btcComponents.symbol} ${btcComponents.value}"
-                    )
-                    LargeRow(
-                        prefix = prefix,
-                        text = converted.formatted,
-                        symbol = converted.symbol,
-                        showSymbol = true
-                    )
-                }
-            }
+        if (primaryDisplay == PrimaryDisplay.BITCOIN) {
+            BalanceHeader(
+                modifier = modifier,
+                smallRowPrefix = prefix,
+                smallRowText = "${converted.symbol} ${converted.formatted}",
+                largeRowPrefix = prefix,
+                largeRowText = btcComponents.value,
+                largeRowSymbol = btcComponents.symbol,
+                showSymbol = showBitcoinSymbol,
+                onClick = { currency.togglePrimaryDisplay() }
+            )
+        } else {
+            BalanceHeader(
+                modifier = modifier,
+                smallRowPrefix = prefix,
+                smallRowText = "${btcComponents.symbol} ${btcComponents.value}",
+                largeRowPrefix = prefix,
+                largeRowText = converted.formatted,
+                largeRowSymbol = converted.symbol,
+                showSymbol = true,
+                onClick = { currency.togglePrimaryDisplay() }
+            )
         }
+    }
+}
+
+@Composable
+fun BalanceHeader(
+    smallRowPrefix: String? = null,
+    smallRowText: String,
+    largeRowPrefix: String? = null,
+    largeRowText: String,
+    largeRowSymbol: String,
+    showSymbol: Boolean,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier.clickableAlpha { onClick() }
+    ) {
+        SmallRow(
+            prefix = smallRowPrefix,
+            text = smallRowText
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LargeRow(
+            prefix = largeRowPrefix,
+            text = largeRowText,
+            symbol = largeRowSymbol,
+            showSymbol = showSymbol
+        )
     }
 }
 
@@ -72,22 +97,19 @@ fun BalanceHeaderView(
 fun LargeRow(prefix: String?, text: String, symbol: String, showSymbol: Boolean) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.height(62.dp)
     ) {
         if (prefix != null) {
             Display(
                 text = prefix,
-                modifier = Modifier
-                    .alpha(0.6f)
-                    .padding(end = 8.dp)
+                color = Colors.White64,
+                modifier = Modifier.padding(end = 8.dp)
             )
         }
         if (showSymbol) {
             Display(
                 text = symbol,
-                modifier = Modifier
-                    .alpha(0.6f)
-                    .padding(end = 8.dp)
+                color = Colors.White64,
+                modifier = Modifier.padding(end = 8.dp)
             )
         }
         Display(text = text)
@@ -97,20 +119,35 @@ fun LargeRow(prefix: String?, text: String, symbol: String, showSymbol: Boolean)
 @Composable
 private fun SmallRow(prefix: String?, text: String) {
     Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .height(24.dp)
-            .padding(bottom = 4.dp)
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
     ) {
         if (prefix != null) {
             Caption13Up(
                 text = prefix,
-                modifier = Modifier.alpha(0.6f)
+                color = Colors.White64,
             )
         }
         Caption13Up(
             text = text,
-            modifier = Modifier.alpha(0.6f)
+            color = Colors.White64,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        BalanceHeader(
+            smallRowPrefix = "$",
+            smallRowText = "27.36",
+            largeRowPrefix = "₿",
+            largeRowText = "136 825",
+            largeRowSymbol = "sats",
+            showSymbol = false,
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {}
         )
     }
 }
