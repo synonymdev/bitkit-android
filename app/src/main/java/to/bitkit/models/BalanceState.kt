51f85ed1eff88483285dbf88bc5166a45a8a97c7
Ovi Trif <ovitrif@proton.me>
2025-03-26 11:40:35 +0100

    feat: Cache balance and load on startup

--- app/src/main/java/to/bitkit/models/BalanceState.kt
+++ app/src/main/java/to/bitkit/models/BalanceState.kt
@@ -1,5 +1,8 @@
 package to.bitkit.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class BalanceState(
     val totalOnchainSats: ULong = 0uL,
     val totalLightningSats: ULong = 0uL,
