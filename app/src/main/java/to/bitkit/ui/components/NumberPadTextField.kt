14021ef1fa25a41bf2a3ed699bf5be75e52db416
Joao Victor Sena <jvsena16@gmail.com>
2025-04-04 13:26:20 -0300

    refactor: clean code

--- app/src/main/java/to/bitkit/ui/components/NumberPadTextField.kt
+++ app/src/main/java/to/bitkit/ui/components/NumberPadTextField.kt
@@ -13,8 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
@@ -23,12 +21,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import to.bitkit.ext.removeSpaces
 import to.bitkit.models.BitcoinDisplayUnit
-import to.bitkit.models.ConvertedAmount
 import to.bitkit.models.PrimaryDisplay
 import to.bitkit.ui.LocalCurrencies
 import to.bitkit.ui.currencyViewModel
 import to.bitkit.ui.theme.Colors
-import kotlin.math.abs
 
 @Composable
 fun NumberPadTextField(
@@ -39,7 +35,6 @@ fun NumberPadTextField(
 ) {
     val (rates, _, _, _, displayUnit, primaryDisplay) = LocalCurrencies.current
     val currency = currencyViewModel ?: return
-
 
     val satoshis by remember {
         derivedStateOf {
@@ -75,7 +70,6 @@ fun NumberPadTextField(
     }
 
     if (input.isNotEmpty()) {
-
         val whole = input.split(".").firstOrNull().orEmpty().removeSpaces()
         val fraction = input.split(".").getOrNull(1).orEmpty().removeSpaces()
 
@@ -108,7 +102,6 @@ fun NumberPadTextField(
         placeholder = placeholder,
         showPlaceholder = true,
         satoshis = satoshis.toIntOrNull() ?: 0,
-        converted = convertedAmount
     )
 }
 
@@ -116,7 +109,6 @@ fun NumberPadTextField(
 fun MoneyAmount(
     modifier: Modifier = Modifier,
     value: String,
-    converted: ConvertedAmount?,
     unit: PrimaryDisplay,
     placeholder: String,
     showPlaceholder: Boolean,
@@ -130,26 +122,8 @@ fun MoneyAmount(
             .semantics { contentDescription = value },
         horizontalAlignment = Alignment.Start
     ) {
-//        if (showConversion && !reverse) {
-//            Money(
-//                sats = satoshis,
-//                showSymbol = true,
-//                unitType = PrimaryDisplay.FIAT,
-//                modifier = Modifier.padding(bottom = 16.dp),
-//                converted = converted
-//            )
-//        }
-//
-//        if (showConversion && reverse) {
-//            Money(
-//                sats = satoshis,
-//                color = Colors.White50,
-//                showSymbol = true,
-//                unitType = PrimaryDisplay.FIAT,
-//                converted = converted
-//            )
-//        }
-        MoneySSB(sats = satoshis.toLong())
+
+        MoneySSB(sats = satoshis.toLong(), reversed = true)
 
         Row(
             verticalAlignment = Alignment.CenterVertically
@@ -183,88 +157,3 @@ fun MoneyAmount(
         }
     }
 }
-
-@Composable
-fun Money(
-    sats: Int,
-    converted: ConvertedAmount?,
-    unitType: PrimaryDisplay = PrimaryDisplay.BITCOIN,
-    unit: PrimaryDisplay? = null,
-    showSymbol: Boolean = false,
-    symbolColor: Color? = null,
-    color: Color? = null,
-    sign: String? = null,
-    shouldRoundUp: Boolean = false,
-    modifier: Modifier = Modifier,
-    testID: String? = null,
-) {
-    val nextUnit = remember { PrimaryDisplay.FIAT } // Replace with actual state management
-
-    val absSats = abs(sats)
-    val actualUnit = unit ?: if (unitType == PrimaryDisplay.FIAT) nextUnit else PrimaryDisplay.BITCOIN
-    val actualShowSymbol = showSymbol || actualUnit == PrimaryDisplay.FIAT
-
-    // Replace with actual display value calculation
-    val displayValues = remember(absSats, shouldRoundUp) {  //TODO IMPLEMENT
-        DisplayValues(
-            bitcoinFormatted = absSats.toString(),
-            fiatFormatted = converted?.formatted.orEmpty(),
-            fiatWhole = converted?.value.toString().orEmpty(),
-            fiatSymbol = converted?.symbol.orEmpty()
-        )
-    }
-
-    val symbol = @Composable {
-        Text(
-            text = if (actualUnit == PrimaryDisplay.BITCOIN) "₿" else displayValues.fiatSymbol,
-            color = symbolColor ?: color ?: Colors.White50,
-        )
-    }
-
-//    val text = remember(actualUnit, denomination, decimalLength, shouldHide) {
-//        when {
-////            shouldHide -> if (size == TextSize.Display) " • • • • • • • • •" else " • • • • •"
-//            actualUnit == PrimaryDisplay.FIAT -> displayValues.fiatFormatted
-//            }
-//            actualUnit == PrimaryDisplay.BITCOIN && denomination == BitcoinDisplayUnit.CLASSIC -> {
-//                if (decimalLength == DecimalLength.Long) {
-//                    "%.8f".format(displayValues.bitcoinFormatted.toDouble())
-//                } else {
-//                    "%.5f".format(displayValues.bitcoinFormatted.toDouble())
-//                }
-//            }
-//            else -> displayValues.bitcoinFormatted
-//        }
-//    }
-
-    val text = remember { displayValues.bitcoinFormatted }
-
-    Row(
-        modifier = modifier
-            .then(testID?.let { Modifier.testTag(it) } ?: Modifier)
-            .semantics { contentDescription = text },
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        if (sign != null) {
-            Text(
-                text = sign,
-                color = color ?: Colors.White50,
-                modifier = Modifier.padding(end = 3.dp),
-            )
-        }
-        if (actualShowSymbol) {
-            symbol()
-        }
-        Text(
-            text = text,
-            color = color ?: Colors.Black50,
-        )
-    }
-}
-
-data class DisplayValues(
-    val bitcoinFormatted: String,
-    val fiatFormatted: String,
-    val fiatWhole: String,
-    val fiatSymbol: String
-)
