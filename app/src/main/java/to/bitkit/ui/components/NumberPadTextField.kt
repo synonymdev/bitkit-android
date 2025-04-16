bc550b67d345733feebee49f0d4fc146d45c3336
Joao Victor Sena <jvsena16@gmail.com>
2025-04-16 11:54:46 -0300

    fix: placeholder glitching

--- app/src/main/java/to/bitkit/ui/components/NumberPadTextField.kt
+++ app/src/main/java/to/bitkit/ui/components/NumberPadTextField.kt
@@ -25,6 +25,7 @@ import to.bitkit.models.formatToModernDisplay
 import to.bitkit.ui.currencyViewModel
 import to.bitkit.ui.theme.Colors
 
+
 @Composable
 fun NumberPadTextField(
     input: String,
@@ -35,7 +36,8 @@ fun NumberPadTextField(
     val currency = currencyViewModel ?: return
 
     val satoshis = if (primaryDisplay == PrimaryDisplay.FIAT) {
-        currency.convertFiatToSats(fiatAmount = input.replace(",", "").toDoubleOrNull() ?: 0.0).toString()
+        currency.convertFiatToSats(fiatAmount = input.replace(",", "").toDoubleOrNull() ?: 0.0)
+            .toString()
     } else {
         input.removeSpaces()
     }
@@ -44,7 +46,7 @@ fun NumberPadTextField(
     var placeholderFractional: String by remember { mutableStateOf("") }
     var value: String by remember { mutableStateOf("") }
 
-    LaunchedEffect (displayUnit, primaryDisplay) {
+    LaunchedEffect(displayUnit, primaryDisplay) {
         placeholderFractional = when {
             displayUnit == BitcoinDisplayUnit.CLASSIC -> "00000000"
             primaryDisplay == PrimaryDisplay.FIAT -> "00"
@@ -52,36 +54,52 @@ fun NumberPadTextField(
         }
 
         placeholder = if (placeholderFractional.isNotEmpty()) {
-            "0.$placeholderFractional"
+            if (input.contains(".") || primaryDisplay == PrimaryDisplay.FIAT) {
+                "0.$placeholderFractional"
+            } else {
+                ".$placeholderFractional"
+            }
         } else {
-            ""
+            "0"
         }
+
+        value = ""
     }
 
     if (input.isNotEmpty()) {
-        val whole = input.split(".").firstOrNull().orEmpty().removeSpaces()
-        val fraction = input.split(".").getOrNull(1).orEmpty().removeSpaces()
+        val parts = input.split(".")
+        val whole = parts.firstOrNull().orEmpty().removeSpaces()
+        val fraction = parts.getOrNull(1).orEmpty().removeSpaces()
 
-        if (primaryDisplay == PrimaryDisplay.FIAT) {
-            value = whole
+        value = when {
+            primaryDisplay == PrimaryDisplay.FIAT -> {
+                if (input.contains(".")) {
+                    "$whole.$fraction"
+                } else {
+                    whole
+                }
+            }
+
+            displayUnit == BitcoinDisplayUnit.MODERN && primaryDisplay == PrimaryDisplay.BITCOIN -> {
+                input.toLongOrDefault(0L).formatToModernDisplay()
+            }
+
+            else -> {
+                whole
+            }
         }
 
-        if (input.contains(".")) {
-            placeholder = ""
-            if (placeholderFractional.length >= fraction.length) {
-                placeholder = placeholderFractional.drop(fraction.length)
+        placeholder = when {
+            input.contains(".") -> {
+                if (fraction.length < placeholderFractional.length) {
+                    placeholderFractional.drop(fraction.length)
+                } else {
+                    ""
+                }
             }
 
-            if (primaryDisplay == PrimaryDisplay.FIAT) {
-                value = "$whole.$fraction"
-            }
-        } else {
-            if (displayUnit == BitcoinDisplayUnit.MODERN && primaryDisplay == PrimaryDisplay.BITCOIN) {
-                value = input.toLongOrDefault(0L).formatToModernDisplay()
-                placeholder = ""
-            } else {
-                placeholder = ".$placeholderFractional"
-            }
+            displayUnit == BitcoinDisplayUnit.MODERN && primaryDisplay == PrimaryDisplay.BITCOIN -> ""
+            else -> if (placeholderFractional.isNotEmpty()) ".$placeholderFractional" else ""
         }
     } else {
         value = ""
