e24038ab21407c72c2e5a198336ef6d6f86d3ce1
Joao Victor Sena <jvsena16@gmail.com>
2025-04-21 08:09:29 -0300

    feat: style

--- app/src/main/java/to/bitkit/ui/screens/wallets/receive/EditInvoiceScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/receive/EditInvoiceScreen.kt
@@ -1,6 +1,5 @@
 package to.bitkit.ui.screens.wallets.receive
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,12 +15,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import to.bitkit.R
-import to.bitkit.models.BalanceState
 import to.bitkit.models.BitcoinDisplayUnit
 import to.bitkit.models.PrimaryDisplay
 import to.bitkit.ui.LocalCurrencies
@@ -35,9 +32,10 @@ import to.bitkit.ui.currencyViewModel
 import to.bitkit.ui.scaffold.SheetTopBar
 import to.bitkit.ui.shared.util.gradientBackground
 import to.bitkit.ui.theme.AppTextFieldDefaults
+import to.bitkit.ui.theme.AppThemeSurface
+import to.bitkit.ui.theme.Colors
 import to.bitkit.viewmodels.CurrencyUiState
 import to.bitkit.viewmodels.SendEvent
-import to.bitkit.viewmodels.SendUiState
 
 @Composable
 fun EditInvoiceScreen(
@@ -47,6 +45,7 @@ fun EditInvoiceScreen(
 ) {
     val currencyVM = currencyViewModel ?: return
     var input: String by remember { mutableStateOf("") }
+    var noteText by remember { mutableStateOf("") }
 
     AmountInputHandler(
         input = input,
@@ -59,23 +58,26 @@ fun EditInvoiceScreen(
 
     EditInvoiceContent(
         input = input,
+        noteText = noteText,
         primaryDisplay = currencyUiState.primaryDisplay,
         displayUnit = currencyUiState.displayUnit,
         onEvent = onEvent,
-        onBack = onBack
+        onBack = onBack,
+        onTextChanged = { newNote -> noteText = newNote }
     )
 }
 
 @Composable
 fun EditInvoiceContent(
     input: String,
+    noteText: String,
     primaryDisplay: PrimaryDisplay,
     displayUnit: BitcoinDisplayUnit,
     onEvent: (SendEvent) -> Unit,
     onBack: () -> Unit,
-){
+    onTextChanged: (String) -> Unit,
+) {
     var keyboardVisible by remember { mutableStateOf(false) }
-    var noteText by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -96,22 +98,31 @@ fun EditInvoiceContent(
                 input = input,
                 displayUnit = displayUnit,
                 primaryDisplay = primaryDisplay,
-                modifier = Modifier.fillMaxWidth().testTag("amount_input_field")
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .testTag("amount_input_field")
             )
             Spacer(modifier = Modifier.height(44.dp))
 
-            Caption13Up(text = stringResource(R.string.wallet__note))
+            Caption13Up(text = stringResource(R.string.wallet__note), color = Colors.White64)
 
             Spacer(modifier = Modifier.height(16.dp))
 
             TextField(
-                placeholder = { BodySSB(stringResource(R.string.wallet__receive_note_placeholder)) },
+                placeholder = {
+                    BodySSB(
+                        text = stringResource(R.string.wallet__receive_note_placeholder),
+                        color = Colors.White64
+                    )
+                },
                 value = noteText,
-                onValueChange = { noteText = it },
+                onValueChange = onTextChanged,
                 minLines = 4,
                 colors = AppTextFieldDefaults.noIndicatorColors,
-                shape = MaterialTheme.shapes.small,
-                modifier = Modifier.fillMaxWidth().defaultMinSize(minHeight = 74.dp),
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .defaultMinSize(minHeight = 74.dp),
             )
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -120,9 +131,44 @@ fun EditInvoiceContent(
 
             PrimaryButton(
                 text = stringResource(R.string.continue_button),
-                onClick = {  }, //TODO IMPLEMENT
+                onClick = { }, //TODO IMPLEMENT
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        EditInvoiceContent(
+            input = "123",
+            noteText = "",
+            primaryDisplay = PrimaryDisplay.BITCOIN,
+            displayUnit = BitcoinDisplayUnit.MODERN,
+            onEvent = {},
+            onBack = {},
+            onTextChanged = {}
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview2() {
+    AppThemeSurface {
+        EditInvoiceContent(
+            input = "123",
+            noteText = "Note text",
+            primaryDisplay = PrimaryDisplay.BITCOIN,
+            displayUnit = BitcoinDisplayUnit.MODERN,
+            onEvent = {},
+            onBack = {},
+            onTextChanged = {}
+        )
     }
 }
 
