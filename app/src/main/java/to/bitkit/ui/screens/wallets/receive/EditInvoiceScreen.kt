472eb4cb780d4cfacb16f0039d23f7eadc7f453f
Joao Victor Sena <jvsena16@gmail.com>
2025-04-22 10:09:10 -0300

    fix: add navigationBarsPadding

--- app/src/main/java/to/bitkit/ui/screens/wallets/receive/EditInvoiceScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/receive/EditInvoiceScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -106,6 +107,7 @@ fun EditInvoiceContent(
         modifier = Modifier
             .fillMaxSize()
             .gradientBackground()
+            .navigationBarsPadding()
             .testTag("edit_invoice_screen")
     ) {
         SheetTopBar(stringResource(R.string.wallet__receive_specify)) {
