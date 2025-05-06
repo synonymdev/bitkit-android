7d3520206f22788bcbc34897099757c88a4e2999
Joao Victor Sena <jvsena16@gmail.com>
2025-05-06 13:49:08 -0300

    fix: set ime option as done

--- app/src/main/java/to/bitkit/ui/screens/wallets/receive/EditInvoiceScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/receive/EditInvoiceScreen.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -32,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import to.bitkit.R
@@ -221,8 +224,11 @@ fun EditInvoiceContent(
                             )
                         },
                         value = noteText,
-                        onValueChange = onTextChanged, //it is being callet with the current state and with the previous state
+                        onValueChange = onTextChanged,
                         minLines = 4,
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Done
+                        ),
                         colors = AppTextFieldDefaults.semiTransparent,
                         shape = MaterialTheme.shapes.medium,
                         modifier = Modifier
