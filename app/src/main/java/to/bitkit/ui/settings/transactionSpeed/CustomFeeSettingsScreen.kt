5cb375015b18c7584b5a09a9f37fda8fe501bb9f
Ovi Trif <ovitrif@proton.me>
2025-05-07 21:54:14 +0200

    feat: Set custom fee average tx text

--- app/src/main/java/to/bitkit/ui/settings/transactionSpeed/CustomFeeSettingsScreen.kt
+++ app/src/main/java/to/bitkit/ui/settings/transactionSpeed/CustomFeeSettingsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,14 +19,18 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import to.bitkit.R
+import to.bitkit.env.Env
 import to.bitkit.models.BITCOIN_SYMBOL
+import to.bitkit.models.ConvertedAmount
 import to.bitkit.models.TransactionSpeed
 import to.bitkit.ui.appViewModel
+import to.bitkit.ui.components.BodyM
 import to.bitkit.ui.components.Caption13Up
 import to.bitkit.ui.components.KEY_DELETE
 import to.bitkit.ui.components.LargeRow
 import to.bitkit.ui.components.PinNumberPad
 import to.bitkit.ui.components.PrimaryButton
+import to.bitkit.ui.currencyViewModel
 import to.bitkit.ui.scaffold.AppTopBar
 import to.bitkit.ui.scaffold.CloseNavIcon
 import to.bitkit.ui.scaffold.ScreenColumn
@@ -41,9 +46,32 @@ fun CustomFeeSettingsScreen(
     var input by remember {
         mutableStateOf((customFeeRate.value as? TransactionSpeed.Custom)?.satsPerVByte?.toString() ?: "")
     }
+    val currency = currencyViewModel ?: return
+    val totalFee = Env.TransactionDefaults.recommendedBaseFee * (input.toUIntOrNull() ?: 0u)
+    var converted: ConvertedAmount? by remember { mutableStateOf(null) }
+
+    LaunchedEffect(input) {
+        val inputNum = input.toLongOrNull() ?: 0
+        converted = if (inputNum == 0L) {
+            null
+        } else {
+            currency.convert(totalFee.toLong())
+        }
+    }
+
+    val totalFeeText = converted
+        ?.let {
+            stringResource(R.string.settings__general__speed_fee_total_fiat)
+                .replace("{feeSats}", "$totalFee")
+                .replace("{fiatSymbol}", it.symbol)
+                .replace("{fiatFormatted}", it.formatted)
+        }
+        ?: stringResource(R.string.settings__general__speed_fee_total)
+            .replace("{feeSats}", "$totalFee")
 
     CustomFeeSettingsContent(
         input = input,
+        totalFeeText = totalFeeText,
         onKeyPress = { key ->
             when (key) {
                 KEY_DELETE -> input = if (input.isNotEmpty()) input.dropLast(1) else ""
@@ -63,12 +91,15 @@ fun CustomFeeSettingsScreen(
 @Composable
 private fun CustomFeeSettingsContent(
     input: String,
+    totalFeeText: String,
     onKeyPress: (String) -> Unit = {},
     onContinue: () -> Unit = {},
     onBackClick: () -> Unit = {},
     onCloseClick: () -> Unit = {},
 ) {
     val feeRate = input.toUIntOrNull() ?: 0u
+    val isValid = feeRate != 0u
+
     ScreenColumn(
         modifier = Modifier.navigationBarsPadding()
     ) {
@@ -83,6 +114,7 @@ private fun CustomFeeSettingsContent(
                 .fillMaxSize()
         ) {
             Caption13Up(text = stringResource(R.string.common__sat_vbyte), color = Colors.White64)
+
             Spacer(modifier = Modifier.height(16.dp))
             LargeRow(
                 prefix = null,
@@ -90,7 +122,11 @@ private fun CustomFeeSettingsContent(
                 symbol = BITCOIN_SYMBOL,
                 showSymbol = true,
             )
-            // TODO add subtitle text (B 250 for average transaction ($10.2)
+
+            if (isValid) {
+                Spacer(modifier = Modifier.height(8.dp))
+                BodyM(totalFeeText, color = Colors.White64)
+            }
 
             Spacer(modifier = Modifier.weight(1f))
 
@@ -100,7 +136,7 @@ private fun CustomFeeSettingsContent(
             )
             PrimaryButton(
                 onClick = onContinue,
-                enabled = feeRate != 0u,
+                enabled = isValid,
                 text = stringResource(R.string.common__continue),
             )
             Spacer(modifier = Modifier.height(16.dp))
@@ -114,6 +150,7 @@ private fun Preview() {
     AppThemeSurface {
         CustomFeeSettingsContent(
             input = "5",
+            totalFeeText = "₿ 256 for average transaction ($0.25)"
         )
     }
 }
