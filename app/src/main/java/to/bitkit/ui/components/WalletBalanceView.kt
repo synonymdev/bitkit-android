f636862d9109da63b89691db36418285f7625f58
Ovi Trif <ovitrif@proton.me>
2025-05-12 12:51:45 +0200

    chore: Cleanup wallet balance view

--- app/src/main/java/to/bitkit/ui/components/WalletBalanceView.kt
+++ app/src/main/java/to/bitkit/ui/components/WalletBalanceView.kt
@@ -12,15 +12,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
 import to.bitkit.models.ConvertedAmount
+import to.bitkit.models.PrimaryDisplay
 import to.bitkit.ui.LocalCurrencies
 import to.bitkit.ui.currencyViewModel
 import to.bitkit.ui.theme.Colors
-import to.bitkit.models.PrimaryDisplay
 
 @Composable
 fun RowScope.WalletBalanceView(
@@ -30,8 +29,8 @@ fun RowScope.WalletBalanceView(
     modifier: Modifier,
 ) {
     val currency = currencyViewModel ?: return
-    val (rates, _, _, _, displayUnit, primaryDisplay) = LocalCurrencies.current
-    val converted: ConvertedAmount? = if (rates.isNotEmpty()) currency.convert(sats = sats) else null
+    val (_, _, _, _, displayUnit, primaryDisplay) = LocalCurrencies.current
+    val converted: ConvertedAmount? = currency.convert(sats = sats)
 
     Column(
         modifier = Modifier
@@ -43,6 +42,7 @@ fun RowScope.WalletBalanceView(
             color = Colors.White64,
         )
         Spacer(modifier = Modifier.height(8.dp))
+
         converted?.let { converted ->
             if (primaryDisplay == PrimaryDisplay.BITCOIN) {
                 val btcComponents = converted.bitcoinDisplay(displayUnit)
