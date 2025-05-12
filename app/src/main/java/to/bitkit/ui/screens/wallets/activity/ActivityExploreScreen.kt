00ac8363d659ef277e5bd49885fd4c2ed6cedd4b
Ovi Trif <ovitrif@proton.me>
2025-05-12 19:33:19 +0200

    feat: Open block explorer testnet support

--- app/src/main/java/to/bitkit/ui/screens/wallets/activity/ActivityExploreScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/activity/ActivityExploreScreen.kt
@@ -20,7 +20,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.lightningdevkit.ldknode.Network
 import to.bitkit.R
+import to.bitkit.env.Env
 import to.bitkit.ext.idValue
 import to.bitkit.ui.Routes
 import to.bitkit.ui.components.BalanceHeaderView
@@ -202,9 +204,13 @@ private fun handleExploreClick(
     onchain: Activity.Onchain,
 ): () -> Unit {
     val context = LocalContext.current
-    val baseUrl = "https://mempool.space"
+    val baseUrl = when(Env.network) {
+        Network.TESTNET -> "https://mempool.space/testnet"
+        else -> "https://mempool.space"
+    }
     val url = "$baseUrl/tx/${onchain.v1.txId}"
     val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+
     return { context.startActivity(intent) }
 }
 
