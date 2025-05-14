8d28540e5799946cda796418430f6630aa36a645
Ovi Trif <ovitrif@proton.me>
2025-05-14 17:37:15 +0200

    feat: Activity filter custom icons

--- app/src/main/java/to/bitkit/ui/screens/wallets/activity/components/ActivityListFilter.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/activity/components/ActivityListFilter.kt
@@ -1,20 +1,16 @@
 package to.bitkit.ui.screens.wallets.activity.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CalendarToday
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Tag
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
@@ -27,9 +23,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import to.bitkit.R
+import to.bitkit.ui.shared.util.clickableAlpha
 import to.bitkit.ui.theme.AppTextFieldDefaults
 import to.bitkit.ui.theme.Colors
 import to.bitkit.viewmodels.ActivityListViewModel
@@ -46,16 +44,17 @@ fun ActivityListFilter(
 
     Column {
         Row(
-            modifier = Modifier
-                .background(Colors.White10, RoundedCornerShape(32.dp))
-                .padding(horizontal = 16.dp)
-                .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .background(Colors.White10, MaterialTheme.shapes.large)
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
         ) {
             Icon(
-                imageVector = Icons.Default.Search,
+                painter = painterResource(R.drawable.ic_magnifying_glass),
                 contentDescription = null,
                 tint = if (searchText.isNotEmpty()) Colors.Brand else Colors.White64,
+                modifier = Modifier.size(24.dp)
             )
             Spacer(modifier = Modifier.width(8.dp))
 
@@ -63,33 +62,29 @@ fun ActivityListFilter(
                 value = searchText,
                 onValueChange = { viewModel.setSearchText(it) },
                 placeholder = { Text(text = stringResource(R.string.common__search)) },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(0.dp),
                 colors = AppTextFieldDefaults.transparent,
+                modifier = Modifier.weight(1f)
             )
             Spacer(modifier = Modifier.width(12.dp))
 
             Row {
                 Icon(
-                    imageVector = Icons.Default.Tag,
+                    painter = painterResource(R.drawable.ic_tag),
                     contentDescription = null,
                     tint = if (selectedTags.isNotEmpty()) Colors.Brand else Colors.White64,
                     modifier = Modifier
-                        .clickable {
-                            onTagClick()
-                        }
+                        .size(24.dp)
+                        .clickableAlpha { onTagClick() }
                 )
                 Spacer(modifier = Modifier.width(12.dp))
 
                 Icon(
-                    imageVector = Icons.Default.CalendarToday,
+                    painter = painterResource(R.drawable.ic_calendar),
                     contentDescription = null,
                     tint = if (startDate != null) Colors.Brand else Colors.White64,
                     modifier = Modifier
-                        .clickable {
-                            onDateRangeClick()
-                        }
+                        .size(24.dp)
+                        .clickableAlpha { onDateRangeClick() }
                 )
             }
         }
