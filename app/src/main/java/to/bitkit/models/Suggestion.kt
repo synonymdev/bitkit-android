310bb10a9e3f781b3f319c82cd63c7e3d060aa0d
Joao Victor Sena <jvsena16@gmail.com>
2025-05-16 14:10:59 -0300

    feat: add icons to suggestions

--- app/src/main/java/to/bitkit/models/Suggestion.kt
+++ app/src/main/java/to/bitkit/models/Suggestion.kt
@@ -9,53 +9,62 @@ import to.bitkit.ui.theme.Colors
 enum class Suggestion(
     @StringRes title: Int,
     @StringRes description: Int,
-    @DrawableRes icon: Int? = null,
+    @DrawableRes icon: Int,
     color: Color
 ) {
     BUY(
         title = R.string.cards__buyBitcoin__title,
         description = R.string.cards__buyBitcoin__description,
-        color = Colors.Brand
+        color = Colors.Brand,
+        icon = R.drawable.b_emboss
     ),
     SPEND(
         title = R.string.cards__lightning__title,
         description = R.string.cards__lightning__description,
-        color = Colors.Purple
+        color = Colors.Purple,
+        icon = R.drawable.lightning
     ),
     BACK_UP(
         title = R.string.cards__backupSeedPhrase__title,
         description = R.string.cards__backupSeedPhrase__description,
-        color = Colors.Blue
+        color = Colors.Blue,
+        icon = R.drawable.safe
     ),
     SECURE(
         title = R.string.cards__pin__title,
         description = R.string.cards__pin__description,
-        color = Colors.Green
+        color = Colors.Green,
+        icon = R.drawable.shield
     ),
     SUPPORT(
         title = R.string.cards__support__title,
         description = R.string.cards__support__description,
-        color = Colors.Yellow
+        color = Colors.Yellow,
+        icon = R.drawable.lightbulb
     ),
     INVITE(
         title = R.string.cards__invite__title,
         description = R.string.cards__invite__description,
-        color = Colors.Blue
+        color = Colors.Blue,
+        icon = R.drawable.group
     ),
     PROFILE(
         title = R.string.cards__slashtagsProfile__title,
         description = R.string.cards__slashtagsProfile__description,
-        color = Colors.Brand
+        color = Colors.Brand,
+        icon = R.drawable.crown
     ),
     SHOP(
         title = R.string.cards__shop__title,
         description = R.string.cards__shop__description,
-        color = Colors.Yellow
+        color = Colors.Yellow,
+        icon = R.drawable.shopping_bag
     ),
     QUICK_PAY(
         title = R.string.cards__quickpay__title,
         description = R.string.cards__shop__description,
-        color = Colors.Green
+        color = Colors.Green,
+        icon = R.drawable.fast_forward
     ),
 }
 
