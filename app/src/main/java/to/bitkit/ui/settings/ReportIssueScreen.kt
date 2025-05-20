c05367d1af867b364501a828b362729a09ddf345
Joao Victor Sena <jvsena16@gmail.com>
2025-05-20 10:02:16 -0300

    feat: ReportIssueScreen.kt WIP

---
+++ app/src/main/java/to/bitkit/ui/settings/ReportIssueScreen.kt
@@ -0,0 +1,60 @@
+package to.bitkit.ui.settings
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import to.bitkit.R
+import to.bitkit.ui.components.BodyM
+import to.bitkit.ui.scaffold.AppTopBar
+import to.bitkit.ui.scaffold.CloseNavIcon
+import to.bitkit.ui.scaffold.ScreenColumn
+import to.bitkit.ui.theme.AppThemeSurface
+import to.bitkit.ui.theme.Colors
+
+
+@Composable
+fun ReportIssueScreen(
+    onBack: () -> Unit,
+    onClose: () -> Unit,
+) {
+    ScreenColumn {
+        AppTopBar(
+            titleText = stringResource(R.string.settings__support__report),
+            onBackClick = onBack,
+            actions = { CloseNavIcon(onClick = onClose) },
+        )
+
+        Column(
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+
+            BodyM(text = stringResource(R.string.settings__support__report_text), color = Colors.White64)
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            BodyM(text = stringResource(R.string.settings__support__label_address), color = Colors.White64)
+
+
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        ReportIssueScreen(
+            onBack = {},
+            onClose = {}
+        )
+    }
+}
