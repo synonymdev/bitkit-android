8b8db6f48fb7edf968b8d9f011632d525eb685c3
Joao Victor Sena <jvsena16@gmail.com>
2025-05-20 10:43:51 -0300

    refactor: create TextInput composable

--- app/src/main/java/to/bitkit/ui/settings/ReportIssueScreen.kt
+++ app/src/main/java/to/bitkit/ui/settings/ReportIssueScreen.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,25 +13,19 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import to.bitkit.R
 import to.bitkit.ext.removeSpaces
 import to.bitkit.ui.components.BodyM
-import to.bitkit.ui.components.BodyMSB
 import to.bitkit.ui.components.PrimaryButton
+import to.bitkit.ui.components.TextInput
 import to.bitkit.ui.scaffold.AppTopBar
 import to.bitkit.ui.scaffold.CloseNavIcon
 import to.bitkit.ui.scaffold.ScreenColumn
-import to.bitkit.ui.theme.AppShapes
-import to.bitkit.ui.theme.AppTextFieldDefaults
 import to.bitkit.ui.theme.AppThemeSurface
 import to.bitkit.ui.theme.Colors
-import to.bitkit.ui.theme.InterFontFamily
 
 
 @Composable
@@ -64,27 +56,11 @@ fun ReportIssueScreen(
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            TextField(
-                placeholder = {
-                    BodyMSB(
-                        stringResource(R.string.settings__support__placeholder_address),
-                        color = Colors.White64
-                    )
-                },
-                textStyle = LocalTextStyle.current.copy(
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 17.sp,
-                    lineHeight = 22.sp,
-                    letterSpacing = 0.4.sp,
-                    fontFamily = InterFontFamily,
-                    textAlign = TextAlign.Start,
-                ),
+            TextInput(
+                placeholder = stringResource(R.string.settings__support__placeholder_address),
                 value = emailInput,
                 onValueChange = { emailInput = emailInput.removeSpaces() },
-                maxLines = 1,
                 singleLine = true,
-                colors = AppTextFieldDefaults.semiTransparent,
-                shape = AppShapes.small,
                 keyboardOptions = KeyboardOptions(
                     imeAction = ImeAction.Next
                 ),
@@ -97,17 +73,10 @@ fun ReportIssueScreen(
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            TextField(
-                placeholder = {
-                    BodyMSB(
-                        stringResource(R.string.settings__support__placeholder_message),
-                        color = Colors.White64
-                    )
-                },
+            TextInput(
+                placeholder = stringResource(R.string.settings__support__placeholder_message),
                 value = emailInput,
                 onValueChange = { emailInput = emailInput.removeSpaces() },
-                colors = AppTextFieldDefaults.semiTransparent,
-                shape = AppShapes.small,
                 modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)
