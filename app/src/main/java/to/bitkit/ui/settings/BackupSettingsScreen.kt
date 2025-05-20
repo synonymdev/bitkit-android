dcc214041d27987be54c4bed7e56bd5abe6b6236
Ovi Trif <ovitrif@proton.me>
2025-05-20 20:31:55 +0200

    feat: Use new settings components in backup settings screen

--- app/src/main/java/to/bitkit/ui/settings/BackupSettingsScreen.kt
+++ app/src/main/java/to/bitkit/ui/settings/BackupSettingsScreen.kt
@@ -1,6 +1,5 @@
 package to.bitkit.ui.settings
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -11,7 +10,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import to.bitkit.R
-import to.bitkit.ui.components.NavButton
+import to.bitkit.ui.components.settings.SettingsButtonRow
 import to.bitkit.ui.navigateToBackupWalletSettings
 import to.bitkit.ui.navigateToRestoreWalletSettings
 import to.bitkit.ui.scaffold.AppTopBar
@@ -24,13 +23,18 @@ fun BackupSettingsScreen(
     ScreenColumn {
         AppTopBar(stringResource(R.string.settings__backup__title), onBackClick = { navController.popBackStack() })
         Column(
-            verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier
                 .padding(horizontal = 16.dp)
                 .verticalScroll(rememberScrollState())
         ) {
-            NavButton(stringResource(R.string.settings__backup__wallet)) { navController.navigateToBackupWalletSettings() }
-            NavButton(stringResource(R.string.settings__backup__reset)) { navController.navigateToRestoreWalletSettings() }
+            SettingsButtonRow(
+                title = stringResource(R.string.settings__backup__wallet),
+                onClick = { navController.navigateToBackupWalletSettings() }
+            )
+            SettingsButtonRow(
+                title = stringResource(R.string.settings__backup__reset),
+                onClick = { navController.navigateToRestoreWalletSettings() }
+            )
         }
     }
 }
