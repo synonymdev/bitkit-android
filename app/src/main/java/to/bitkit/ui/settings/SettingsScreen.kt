fc023cd44528b52f221c7c731041a98bde5210d2
Ovi Trif <ovitrif@proton.me>
2025-05-21 13:39:58 +0200

    feat: Cog image on settings screen

--- app/src/main/java/to/bitkit/ui/settings/SettingsScreen.kt
+++ app/src/main/java/to/bitkit/ui/settings/SettingsScreen.kt
@@ -1,11 +1,17 @@
 package to.bitkit.ui.settings
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -41,6 +47,7 @@ fun SettingsScreen(
         Column(
             modifier = Modifier
                 .padding(horizontal = 16.dp)
+                .fillMaxSize()
                 .verticalScroll(rememberScrollState())
         ) {
             SettingsButtonRow(
@@ -95,6 +102,16 @@ fun SettingsScreen(
                 SettingsTextButtonRow("Generate Test Activities") { activity.generateRandomTestData() }
                 SettingsTextButtonRow(stringResource(R.string.security__wipe_app)) { viewModel.wipeStorage() }
             }
+
+            Spacer(Modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.cog),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(256.dp)
+            )
+            Spacer(Modifier.weight(1f))
         }
     }
 }
