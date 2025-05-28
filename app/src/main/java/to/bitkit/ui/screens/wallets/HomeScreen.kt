a36075f3ce3905b31f4b0b9ed3458db1b3a2daf8
Joao Victor Sena <jvsena16@gmail.com>
2025-05-28 11:08:46 -0300

    refactor: use material 3 icon

--- app/src/main/java/to/bitkit/ui/screens/wallets/HomeScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/HomeScreen.kt
@@ -24,11 +24,11 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
