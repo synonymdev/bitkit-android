d5d2497baa69f4db4998989c15a5625b71dbfafd
Ovi Trif <ovitrif@proton.me>
2025-06-02 16:44:57 +0200

    refactor: Split backup settings content view

--- app/src/main/java/to/bitkit/ui/settings/BackupSettingsScreen.kt
+++ app/src/main/java/to/bitkit/ui/settings/BackupSettingsScreen.kt
@@ -7,21 +7,44 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import to.bitkit.R
 import to.bitkit.ui.components.settings.SettingsButtonRow
 import to.bitkit.ui.navigateToBackupWalletSettings
+import to.bitkit.ui.navigateToHome
 import to.bitkit.ui.navigateToRestoreWalletSettings
 import to.bitkit.ui.scaffold.AppTopBar
+import to.bitkit.ui.scaffold.CloseNavIcon
 import to.bitkit.ui.scaffold.ScreenColumn
+import to.bitkit.ui.theme.AppThemeSurface
 
 @Composable
 fun BackupSettingsScreen(
     navController: NavController,
 ) {
+    BackupSettingsScreenContent(
+        onBackupClick = { navController.navigateToBackupWalletSettings() },
+        onResetAndRestoreClick = { navController.navigateToRestoreWalletSettings() },
+        onBack = { navController.popBackStack() },
+        onClose = { navController.navigateToHome() },
+    )
+}
+
+@Composable
+private fun BackupSettingsScreenContent(
+    onBackupClick: () -> Unit,
+    onResetAndRestoreClick: () -> Unit,
+    onBack: () -> Unit,
+    onClose: () -> Unit,
+) {
     ScreenColumn {
-        AppTopBar(stringResource(R.string.settings__backup__title), onBackClick = { navController.popBackStack() })
+        AppTopBar(
+            titleText = stringResource(R.string.settings__backup__title),
+            onBackClick = onBack,
+            actions = { CloseNavIcon(onClick = onClose) },
+        )
         Column(
             modifier = Modifier
                 .padding(horizontal = 16.dp)
@@ -29,12 +52,25 @@ fun BackupSettingsScreen(
         ) {
             SettingsButtonRow(
                 title = stringResource(R.string.settings__backup__wallet),
-                onClick = { navController.navigateToBackupWalletSettings() }
+                onClick = onBackupClick,
             )
             SettingsButtonRow(
                 title = stringResource(R.string.settings__backup__reset),
-                onClick = { navController.navigateToRestoreWalletSettings() }
+                onClick = onResetAndRestoreClick,
             )
         }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        BackupSettingsScreenContent(
+            onBackupClick = {},
+            onResetAndRestoreClick = {},
+            onBack = {},
+            onClose = {},
+        )
     }
 }
