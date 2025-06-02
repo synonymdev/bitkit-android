9803f18b3c977e4a8fe0ed46d7ac921cd68339a3
Joao Victor Sena <jvsena16@gmail.com>
2025-06-02 13:09:13 -0300

    feat: facts cards

---
+++ app/src/main/java/to/bitkit/ui/screens/widgets/facts/FacsCard.kt
@@ -0,0 +1,138 @@
+package to.bitkit.ui.screens.widgets.facts
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import to.bitkit.R
+import to.bitkit.ui.components.BodyMB
+import to.bitkit.ui.components.BodyMSB
+import to.bitkit.ui.components.BodyS
+import to.bitkit.ui.theme.AppThemeSurface
+import to.bitkit.ui.theme.Colors
+
+@Composable
+fun FactsCard(
+    modifier: Modifier = Modifier,
+    showWidgetTitle: Boolean = true,
+    showSource: Boolean = true,
+    headline: String,
+    source: String,
+) {
+
+    Box(
+        modifier = modifier
+            .clip(shape = MaterialTheme.shapes.medium)
+            .background(Colors.White10)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            if (showWidgetTitle) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.testTag("widget_title_row")
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.widget_lightbulb),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(32.dp)
+                            .testTag("widget_title_icon"),
+                        tint = Color.Unspecified
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    BodyMSB(
+                        text = stringResource(R.string.widgets__facts__name),
+                        modifier = Modifier.testTag("widget_title_text")
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            BodyMB(
+                text = headline,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.testTag("headline_text")
+            )
+
+            if (showSource) {
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .testTag("source_row"),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    BodyS(
+                        text = stringResource(R.string.widgets__widget__source),
+                        color = Colors.White64,
+                        modifier = Modifier.testTag("source_label")
+                    )
+                    BodyS(
+                        text = source,
+                        color = Colors.White64,
+                        modifier = Modifier.testTag("source_text")
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            FactsCard(
+                showWidgetTitle = true,
+                showSource = true,
+                headline = "Priced in Bitcoin, products can become cheaper over time.",
+                source = "synonym.to",
+            )
+            FactsCard(
+                showWidgetTitle = true,
+                showSource = false,
+                headline = "Priced in Bitcoin, products can become cheaper over time.",
+                source = "synonym.to",
+            )
+            FactsCard(
+                showWidgetTitle = false,
+                showSource = false,
+                headline = "Priced in Bitcoin, products can become cheaper over time.",
+                source = "synonym.to",
+            )
+        }
+    }
+}
