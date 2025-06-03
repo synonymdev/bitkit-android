f71f7fe39c322ac38035e976c50bc21a7f295725
Joao Victor Sena <jvsena16@gmail.com>
2025-06-03 13:20:22 -0300

    feat: BlocksPreferences

---
+++ app/src/main/java/to/bitkit/models/widget/BlocksPreferences.kt
@@ -0,0 +1,14 @@
+package to.bitkit.models.widget
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BlocksPreferences(
+    val showBlock: Boolean = true,
+    val showTime: Boolean = true,
+    val showDate: Boolean = true,
+    val showTransactions: Boolean = false,
+    val showSize: Boolean = false,
+    val showFees: Boolean = false,
+    val showSource: Boolean = false,
+)
