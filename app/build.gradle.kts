6a3ca3536fe3fc84a2dd05524c6b90c810ca5ffd
Ovi Trif <ovitrif@proton.me>
2025-06-06 20:26:07 +0200

    chore: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "to.bitkit.dev"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
         testInstrumentationRunner = "to.bitkit.test.HiltTestRunner"
         vectorDrawables {
             useSupportLibrary = true
