5ab489b5e0dd0745b23facb7f5e6a6e7f331ec77
Ovi Trif <ovitrif@proton.me>
2025-06-13 13:15:39 +0200

    chore: Remove unused import

--- app/src/main/java/to/bitkit/ui/settings/BackupSettingsScreen.kt
+++ app/src/main/java/to/bitkit/ui/settings/BackupSettingsScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import to.bitkit.R
