8fc9958d99ce6e44b8804115e82a78ace7b44aa9
Ovi Trif <ovitrif@proton.me>
2025-06-13 17:15:40 +0200

    feat: Enhance subscreens scaffold

--- app/src/main/java/to/bitkit/ui/settings/advanced/AddressViewerScreen.kt
+++ app/src/main/java/to/bitkit/ui/settings/advanced/AddressViewerScreen.kt
@@ -1,13 +1,17 @@
 package to.bitkit.ui.settings.advanced
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import to.bitkit.R
 import to.bitkit.ui.navigateToHome
@@ -37,8 +41,19 @@ private fun Content(
             onBackClick = onBack,
             actions = { CloseNavIcon(onClose) },
         )
-        Box(modifier = Modifier.fillMaxSize()) {
-            Text("TODO: Address Viewer Screen", modifier = Modifier.align(Alignment.Center))
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxSize()
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                Text("TODO: Address Viewer Screen")
+            }
         }
     }
 }
