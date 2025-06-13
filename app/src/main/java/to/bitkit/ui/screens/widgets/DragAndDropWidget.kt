1059c436df3968ee107775299d02ee03ea1cb412
Joao Victor Sena <jvsena16@gmail.com>
2025-06-13 14:01:50 -0300

    feat: DragAndDropWidget.kt

---
+++ app/src/main/java/to/bitkit/ui/screens/widgets/DragAndDropWidget.kt
@@ -0,0 +1,92 @@
+package to.bitkit.ui.screens.widgets
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import to.bitkit.R
+import to.bitkit.ui.components.BodyMSB
+import to.bitkit.ui.theme.Colors
+
+@Composable
+fun DragAndDropWidget(
+    @DrawableRes iconRes: Int,
+    title: String,
+    onClickDelete: () -> Unit,
+    onClickSettings: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .clip(shape = MaterialTheme.shapes.medium)
+            .background(Colors.White10)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(iconRes),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(32.dp)
+                    .testTag("${title}_drag_and_drop_icon"),
+                tint = Color.Unspecified
+            )
+
+            BodyMSB(
+                text = title,
+                modifier = Modifier
+                    .weight(1f)
+                    .testTag("${title}_drag_and_drop_title")
+            )
+
+            IconButton(
+                onClick = onClickDelete,
+                modifier = Modifier.testTag("${title}_drag_and_drop_delete")
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_trash),
+                    contentDescription = stringResource(R.string.common__delete)
+                )
+            }
+
+            IconButton(
+                onClick = onClickSettings,
+                modifier = Modifier.testTag("${title}_drag_and_drop_edit")
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_settings),
+                    contentDescription = stringResource(R.string.common__edit)
+                )
+            }
+
+            IconButton(
+                onClick = onClickDelete,
+                modifier = Modifier.testTag("${title}_drag_and_drop_move")
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_list),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
