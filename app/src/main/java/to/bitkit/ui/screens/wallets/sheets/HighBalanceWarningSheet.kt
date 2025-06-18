61c341daac7aba82b6be10c43e115cc78ab56405
Joao Victor Sena <jvsena16@gmail.com>
2025-06-18 07:36:37 -0300

    feat: HighBalanceWarningSheet.kt

---
+++ app/src/main/java/to/bitkit/ui/screens/wallets/sheets/HighBalanceWarningSheet.kt
@@ -0,0 +1,125 @@
+package to.bitkit.ui.screens.wallets.sheets
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import to.bitkit.R
+import to.bitkit.ui.components.BodyM
+import to.bitkit.ui.components.Display
+import to.bitkit.ui.components.PrimaryButton
+import to.bitkit.ui.components.SecondaryButton
+import to.bitkit.ui.scaffold.ScreenColumn
+import to.bitkit.ui.scaffold.SheetTopBar
+import to.bitkit.ui.shared.util.gradientBackground
+import to.bitkit.ui.theme.AppThemeSurface
+import to.bitkit.ui.theme.Colors
+import to.bitkit.ui.theme.InterFontFamily
+import to.bitkit.ui.utils.withAccent
+
+@Composable
+fun HighBalanceWarningSheet(
+    understoodClick: () -> Unit,
+    learnMoreClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    ScreenColumn(
+        modifier = modifier
+            .gradientBackground()
+            .testTag("high_balance_intro_screen")
+    ) {
+        SheetTopBar(stringResource(R.string.other__high_balance__nav_title))
+
+        Column(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.exclamation_mark),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .testTag("high_balance_image")
+            )
+
+            Display(
+                text = stringResource(R.string.other__high_balance__title).withAccent(accentColor = Colors.Yellow),
+                color = Colors.White,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .testTag("high_balance_title")
+            )
+            Spacer(Modifier.height(8.dp))
+            BodyM(
+                text =
+                    stringResource(R.string.other__high_balance__text).withAccent(
+                        defaultColor = Colors.White64,
+                        accentStyle = SpanStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 17.sp,
+                            letterSpacing = 0.4.sp,
+                            fontFamily = InterFontFamily,
+                            color = Colors.White,
+                        )
+                    ),
+                color = Colors.White64,
+                modifier = Modifier
+                    .testTag("high_balance_description")
+            )
+            Spacer(Modifier.height(32.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .testTag("buttons_row"),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                SecondaryButton(
+                    text = stringResource(R.string.other__high_balance__cancel),
+                    fullWidth = false,
+                    onClick = learnMoreClick,
+                    modifier = Modifier
+                        .weight(1f)
+                        .testTag("learn_more_button"),
+                )
+
+                PrimaryButton(
+                    text = stringResource(R.string.other__high_balance__continue),
+                    fullWidth = false,
+                    onClick = understoodClick,
+                    modifier = Modifier
+                        .weight(1f)
+                        .testTag("understood_button"),
+                )
+            }
+            Spacer(Modifier.height(16.dp))
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        HighBalanceWarningSheet(
+            understoodClick = {},
+            learnMoreClick = {}
+        )
+    }
+}
