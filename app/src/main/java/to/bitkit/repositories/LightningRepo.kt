cfe362e9630cc6368c85eca403534efb7711954f
Joao Victor Sena <jvsena16@gmail.com>
2025-07-02 10:00:24 -0300

    feat: improve logs

--- app/src/main/java/to/bitkit/repositories/LightningRepo.kt
+++ app/src/main/java/to/bitkit/repositories/LightningRepo.kt
@@ -543,7 +543,7 @@ class LightningRepo @Inject constructor(
             )
             Result.success(replacementTxId)
         } catch (e: Throwable) {
-            Logger.error("bumpFeeByRbf error", e, context = TAG)
+            Logger.error("bumpFeeByRbf error originalTxId: $originalTxId, satsPerVByte: $satsPerVByte", e, context = TAG)
             Result.failure(e)
         }
     }
