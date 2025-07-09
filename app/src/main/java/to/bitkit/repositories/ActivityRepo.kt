341fafd78532252d02181ba3efefbd76c455c8b0
Joao Victor Sena <jvsena16@gmail.com>
2025-07-09 06:51:53 -0300

    feat: findActivityByPaymentId method

--- app/src/main/java/to/bitkit/repositories/ActivityRepo.kt
+++ app/src/main/java/to/bitkit/repositories/ActivityRepo.kt
@@ -1,8 +1,13 @@
 package to.bitkit.repositories
 
+import com.synonym.bitkitcore.Activity
+import com.synonym.bitkitcore.ActivityFilter
+import com.synonym.bitkitcore.PaymentType
+import com.synonym.bitkitcore.SortDirection
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import to.bitkit.di.BgDispatcher
+import to.bitkit.ext.matchesPaymentId
 import to.bitkit.services.CoreService
 import to.bitkit.utils.Logger
 import javax.inject.Inject
@@ -39,6 +44,71 @@ class ActivityRepo @Inject constructor(
                 }.map { Unit }
         }.onFailure { e ->
             Logger.error("syncLdkNodePayments error", e, context = TAG)
+        }
+    }
+
+    /**
+     * Gets a specific activity by payment hash or txID
+     */
+    suspend fun findActivityByPaymentId(
+        paymentHashOrTxId: String,
+        type: ActivityFilter,
+        txType: PaymentType,
+    ): Result<Activity> = withContext(bgDispatcher) {
+
+        return@withContext try {
+            suspend fun findActivity(): Activity? = getActivities(
+                filter = type,
+                txType = txType,
+                limit = 10u
+            ).getOrNull()?.firstOrNull { it.matchesPaymentId(paymentHashOrTxId) }
+
+            var activity = findActivity()
+            if (activity == null) {
+                Logger.warn(
+                    "activity with paymentHashOrTxId:$paymentHashOrTxId not found, trying again after sync",
+                    context = TAG
+                )
+                syncActivities().onSuccess {
+                    Logger.debug(
+                        "Sync success, searching again the activity with paymentHashOrTxId:$paymentHashOrTxId",
+                        context = TAG
+                    )
+                    activity = findActivity()
+                }
+            }
+
+            if (activity != null) Result.success(activity) else Result.failure(IllegalStateException("Activity not found"))
+        } catch (e: Exception) {
+            Logger.error(
+                "findActivityByPaymentId error. Parameters:\n paymentHashOrTxId:$paymentHashOrTxId type:$type txType:$txType",
+                context = TAG
+            )
+            Result.failure(e)
+        }
+    }
+
+    /**
+     * Gets activities with specified filters
+     */
+    suspend fun getActivities(
+        filter: ActivityFilter? = null,
+        txType: PaymentType? = null,
+        tags: List<String>? = null,
+        search: String? = null,
+        minDate: ULong? = null,
+        maxDate: ULong? = null,
+        limit: UInt? = null,
+        sortDirection: SortDirection? = null,
+    ): Result<List<Activity>> = withContext(bgDispatcher) {
+        return@withContext runCatching {
+            coreService.activity.get(filter, txType, tags, search, minDate, maxDate, limit, sortDirection)
+        }.onFailure { e ->
+            Logger.error(
+                "getActivities error. Parameters:\nfilter:$filter txType:$txType tags:$tags search:$search minDate:$minDate maxDate:$maxDate limit:$limit sortDirection:$sortDirection",
+                e = e,
+                context = TAG
+            )
         }
     }
 
