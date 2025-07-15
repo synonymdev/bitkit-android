432c1c1648915335076bf4070478e5f425dacf98
Joao Victor Sena <jvsena16@gmail.com>
2025-07-15 14:19:40 -0300

    chore: remove import

--- app/src/main/java/to/bitkit/services/LnUrlWithdrawService.kt
+++ app/src/main/java/to/bitkit/services/LnUrlWithdrawService.kt
@@ -3,7 +3,6 @@ package to.bitkit.services
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import io.ktor.client.request.parameter
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.isSuccess
 import kotlinx.serialization.Serializable
