485de2e33fc6f28b66f3276b8df563bb6db04b04
Ovi Trif <ovitrif@proton.me>
2025-07-19 17:24:14 +0200

    feat: tabBar haze polish

--- app/src/main/java/to/bitkit/ui/components/TabBar.kt
+++ app/src/main/java/to/bitkit/ui/components/TabBar.kt
@@ -3,9 +3,9 @@ package to.bitkit.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,10 +18,10 @@ import androidx.compose.material.icons.filled.ArrowDownward
 import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -32,16 +32,19 @@ import dev.chrisbanes.haze.hazeEffect
 import dev.chrisbanes.haze.hazeSource
 import dev.chrisbanes.haze.materials.CupertinoMaterials
 import dev.chrisbanes.haze.materials.ExperimentalHazeMaterialsApi
+import dev.chrisbanes.haze.rememberHazeState
 import to.bitkit.R
 import to.bitkit.ui.theme.AppThemeSurface
 import to.bitkit.ui.theme.Colors
 
 private val buttonBg = Color(40, 40, 40).copy(alpha = 0.95f)
-private val buttonBgOverlay = buttonBg.copy(alpha = 0.5f)
+private val buttonBgOverlay = Brush.verticalGradient(colors = listOf(buttonBg, Color.Transparent))
+
 private val iconToTextGap = 4.dp
 private val iconSize = 20.dp
-private val ButtonLeftShape = RoundedCornerShape(topStartPercent = 50, bottomStartPercent = 50)
-private val ButtonRightShape = RoundedCornerShape(topEndPercent = 50, bottomEndPercent = 50)
+
+private val buttonLeftShape = RoundedCornerShape(topStartPercent = 50, bottomStartPercent = 50)
+private val buttonRightShape = RoundedCornerShape(topEndPercent = 50, bottomEndPercent = 50)
 
 @OptIn(ExperimentalHazeMaterialsApi::class)
 @Composable
@@ -56,6 +59,14 @@ fun TabBar(
         contentAlignment = Alignment.Center,
         modifier = modifier
             .fillMaxWidth()
+            .background(
+                Brush.verticalGradient(
+                    colorStops = arrayOf(
+                        0.0f to Color.Transparent,
+                        0.5f to Colors.Black,
+                    ),
+                )
+            )
             .padding(horizontal = 16.dp)
             .padding(bottom = 16.dp)
     ) {
@@ -67,14 +78,17 @@ fun TabBar(
                 modifier = Modifier
                     .weight(1f)
                     .height(60.dp)
-                    .clip(ButtonLeftShape)
+                    .clip(buttonLeftShape)
                     .background(buttonBg) // fallback if no haze
-                    .hazeSource(hazeState, zIndex = 1f)
                     .hazeEffect(
                         state = hazeState,
                         style = buttonMaterial,
                     )
-                    .background(buttonBgOverlay)
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(buttonBg, Color.Transparent),
+                        )
+                    )
                     .clickable { onSendClick() }
             ) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
@@ -94,9 +108,8 @@ fun TabBar(
                 modifier = Modifier
                     .weight(1f)
                     .height(60.dp)
-                    .clip(ButtonRightShape)
+                    .clip(buttonRightShape)
                     .background(buttonBg) // fallback if no haze
-                    .hazeSource(hazeState, zIndex = 1f)
                     .hazeEffect(
                         state = hazeState,
                         style = buttonMaterial,
@@ -117,55 +130,71 @@ fun TabBar(
         }
 
         // Scan Button
-        val scanBg = Colors.Gray6
+        val scanBg = Colors.Gray6.copy(alpha = 0.75f)
+        // Outer Border
         Box(
+            content = {},
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .size(80.dp)
                 .clip(CircleShape)
-                .hazeSource(hazeState, zIndex = 2f)
                 .background(buttonBg) // fallback if no haze
                 .hazeEffect(
                     state = hazeState,
                     style = CupertinoMaterials.regular(containerColor = buttonBg),
                 )
-                .background(buttonBgOverlay)
+                .background(
+                    Brush.verticalGradient(colors = listOf(Colors.White10, Color.Transparent))
+                )
                 .clickable { onScanClick() }
                 .padding(2.dp)
-        ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clip(CircleShape)
-                    .background(scanBg) // fallback if no haze
-                    .hazeSource(hazeState, zIndex = 2f)
-                    .hazeEffect(
-                        state = hazeState,
-                        style = CupertinoMaterials.thick(containerColor = scanBg),
-                    )
-                    .background(Colors.Black.copy(alpha = 0.4f))
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_scan),
-                    contentDescription = stringResource(R.string.wallet__recipient_scan),
-                    tint = Colors.Gray2,
-                    modifier = Modifier.size(32.dp)
+        )
+        // Inner Content
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(76.dp)
+                .clip(CircleShape)
+                .background(buttonBg) // fallback if no haze
+                .hazeEffect(
+                    state = hazeState,
+                    style = CupertinoMaterials.regular(containerColor = Color.Transparent),
                 )
-            }
+                .background(scanBg)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_scan),
+                contentDescription = stringResource(R.string.wallet__recipient_scan),
+                tint = Colors.Gray2,
+                modifier = Modifier.size(32.dp)
+            )
         }
     }
 }
 
 @Preview()
 @Composable
-private fun TabBarPreview() {
+private fun Preview() {
     AppThemeSurface {
-        TabBar(
-            onSendClick = {},
-            onReceiveClick = {},
-            onScanClick = {},
-            hazeState = remember { HazeState() },
-        )
+        val hazeState = rememberHazeState()
+        Box(
+            modifier = Modifier.background(Colors.Brand)
+        ) {
+            // Content Behind
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .hazeSource(hazeState, zIndex = 0f)
+            ) {
+                BodyMB("Some text behind the footer bar to simulate content.")
+                BodyM("Additional random text for a second line of content.")
+            }
+            TabBar(
+                onSendClick = {},
+                onReceiveClick = {},
+                onScanClick = {},
+                hazeState = hazeState,
+            )
+        }
     }
 }
