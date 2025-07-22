bc54c876e5335cff0eeb45efb53d050c150be586
Joao Victor Sena <jvsena16@gmail.com>
2025-07-22 10:01:34 -0300

    feat: add AppTopBar

--- app/src/main/java/to/bitkit/ui/screens/shop/ShopDiscoverScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/shop/ShopDiscoverScreen.kt
@@ -6,17 +6,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,12 +44,19 @@ import to.bitkit.ui.shared.util.gradientBackground
 import to.bitkit.ui.theme.AppThemeSurface
 import to.bitkit.ui.theme.Colors
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShopDiscoverScreen(
     onClose: () -> Unit,
     onBack: () -> Unit,
     navigateWebView: (String, String) -> Unit, //Page, Title
 ) {
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+    val tabTitles = listOf(
+        stringResource(R.string.other__shop__discover__tabs__shop),
+        stringResource(R.string.other__shop__discover__tabs__map)
+    )
+
     ScreenColumn(
         modifier = Modifier.gradientBackground(),
     ) {
@@ -49,124 +66,163 @@ fun ShopDiscoverScreen(
             actions = { CloseNavIcon(onClick = onClose) },
         )
 
-        LazyColumn(
-            modifier = Modifier.padding(horizontal = 16.dp),
+        PrimaryTabRow(
+            selectedTabIndex = selectedTabIndex,
+            containerColor = Color.Transparent,
+            modifier = Modifier.padding(horizontal = 16.dp)
         ) {
-            item {
-                VerticalSpacer(16.dp)
-
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                ) {
-                    val title = stringResource(R.string.other__shop__discover__gift_cards__title)
-                    SuggestionCard(
-                        modifier = Modifier.weight(1f),
-                        gradientColor = Colors.Green,
-                        title = title,
-                        description = stringResource(R.string.other__shop__discover__gift_cards__description),
-                        icon = R.drawable.gift,
-                        captionColor = Colors.Gray1,
-                        size = 164,
-                        onClick = {
-                            navigateWebView("gift-cards", title)
-                        },
-                    )
-                    val title2 = stringResource(R.string.other__shop__discover__esims__title)
-                    SuggestionCard(
-                        modifier = Modifier.weight(1f),
-                        gradientColor = Colors.Yellow,
-                        title = title2,
-                        description = stringResource(R.string.other__shop__discover__esims__description),
-                        icon = R.drawable.globe,
-                        captionColor = Colors.Gray1,
-                        size = 164,
-                        onClick = {
-                            navigateWebView("esims", title2)
-                        },
-                    )
-                }
-
-                VerticalSpacer(16.dp)
-
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                ) {
-                    val title = stringResource(R.string.other__shop__discover__refill__title)
-                    SuggestionCard(
-                        modifier = Modifier.weight(1f),
-                        gradientColor = Colors.Purple,
-                        title = title,
-                        description = stringResource(R.string.other__shop__discover__refill__description),
-                        icon = R.drawable.phone,
-                        captionColor = Colors.Gray1,
-                        size = 164,
-                        onClick = {
-                            navigateWebView("refill", title)
-                        },
-                    )
-                    val title2 = stringResource(R.string.other__shop__discover__travel__title)
-                    SuggestionCard(
-                        modifier = Modifier.weight(1f),
-                        gradientColor = Colors.Red,
-                        title = title2,
-                        description = stringResource(R.string.other__shop__discover__travel__description),
-                        icon = R.drawable.rocket_2,
-                        size = 164,
-                        captionColor = Colors.Gray1,
-                        onClick = {
-                            navigateWebView("buy/travel", title2)
-                        },
-                    )
-                }
-
-                VerticalSpacer(32.dp)
-
-                Text13Up(stringResource(R.string.other__shop__discover__label), color = Colors.White64)
-
-                VerticalSpacer(16.dp)
-            }
-
-            items(items = BitrefillCategory.entries.toList(), key = { it.name }) { item ->
-                Column {
-                    Row(
-                        modifier = Modifier
-                            .padding(top = 8.5.dp, bottom = 10.5.dp)
-                            .clickable {
-                                navigateWebView(item.route, item.title)
-                            },
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .clip(CircleShape)
-                                .size(32.dp)
-                                .background(Colors.White10),
-                            contentAlignment = Alignment.Center,
-                        ) {
-                            Icon(
-                                imageVector = item.icon,
-                                contentDescription = null,
-                                tint = Colors.White64,
-                                modifier = Modifier.size(16.dp),
-                            )
-                        }
-                        BodyM(
-                            text = item.title,
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 8.dp),
-                        )
-                        Icon(
-                            painter = painterResource(R.drawable.ic_chevron_right),
-                            contentDescription = null,
-                            tint = Colors.White64,
-                            modifier = Modifier.size(24.dp),
-                        )
-                    }
-                    HorizontalDivider()
-                }
+            tabTitles.forEachIndexed { index, title ->
+                Tab(
+                    selected = selectedTabIndex == index,
+                    onClick = { selectedTabIndex = index },
+                    text = { Text(title) }
+                )
             }
         }
+
+        when (selectedTabIndex) {
+            0 -> ShopTabContent(navigateWebView = navigateWebView)
+            1 -> MapTabContent()
+        }
+    }
+}
+
+@Composable
+private fun ShopTabContent(
+    navigateWebView: (String, String) -> Unit
+) {
+    LazyColumn(
+        modifier = Modifier.padding(horizontal = 16.dp),
+    ) {
+        item {
+            VerticalSpacer(16.dp)
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                val title = stringResource(R.string.other__shop__discover__gift_cards__title)
+                SuggestionCard(
+                    modifier = Modifier.weight(1f),
+                    gradientColor = Colors.Green,
+                    title = title,
+                    description = stringResource(R.string.other__shop__discover__gift_cards__description),
+                    icon = R.drawable.gift,
+                    captionColor = Colors.Gray1,
+                    size = 164,
+                    onClick = {
+                        navigateWebView("gift-cards", title)
+                    },
+                )
+                val title2 = stringResource(R.string.other__shop__discover__esims__title)
+                SuggestionCard(
+                    modifier = Modifier.weight(1f),
+                    gradientColor = Colors.Yellow,
+                    title = title2,
+                    description = stringResource(R.string.other__shop__discover__esims__description),
+                    icon = R.drawable.globe,
+                    captionColor = Colors.Gray1,
+                    size = 164,
+                    onClick = {
+                        navigateWebView("esims", title2)
+                    },
+                )
+            }
+
+            VerticalSpacer(16.dp)
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                val title = stringResource(R.string.other__shop__discover__refill__title)
+                SuggestionCard(
+                    modifier = Modifier.weight(1f),
+                    gradientColor = Colors.Purple,
+                    title = title,
+                    description = stringResource(R.string.other__shop__discover__refill__description),
+                    icon = R.drawable.phone,
+                    captionColor = Colors.Gray1,
+                    size = 164,
+                    onClick = {
+                        navigateWebView("refill", title)
+                    },
+                )
+                val title2 = stringResource(R.string.other__shop__discover__travel__title)
+                SuggestionCard(
+                    modifier = Modifier.weight(1f),
+                    gradientColor = Colors.Red,
+                    title = title2,
+                    description = stringResource(R.string.other__shop__discover__travel__description),
+                    icon = R.drawable.rocket_2,
+                    size = 164,
+                    captionColor = Colors.Gray1,
+                    onClick = {
+                        navigateWebView("buy/travel", title2)
+                    },
+                )
+            }
+
+            VerticalSpacer(32.dp)
+
+            Text13Up(stringResource(R.string.other__shop__discover__label), color = Colors.White64)
+
+            VerticalSpacer(16.dp)
+        }
+
+        items(items = BitrefillCategory.entries.toList(), key = { it.name }) { item ->
+            Column {
+                Row(
+                    modifier = Modifier
+                        .padding(top = 8.5.dp, bottom = 10.5.dp)
+                        .clickable {
+                            navigateWebView(item.route, item.title)
+                        },
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .size(32.dp)
+                            .background(Colors.White10),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = null,
+                            tint = Colors.White64,
+                            modifier = Modifier.size(16.dp),
+                        )
+                    }
+                    BodyM(
+                        text = item.title,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = 8.dp),
+                    )
+                    Icon(
+                        painter = painterResource(R.drawable.ic_chevron_right),
+                        contentDescription = null,
+                        tint = Colors.White64,
+                        modifier = Modifier.size(24.dp),
+                    )
+                }
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
+@Composable
+private fun MapTabContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Map content coming soon",
+            color = Colors.White64
+        )
     }
 }
 
