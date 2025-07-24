d68db53a303fda4fd2e0d8f2f56dc2a04ec4c3f5
Ovi Trif <ovitrif@proton.me>
2025-07-24 20:53:22 +0200

    fix: remember amount value on send screen

--- app/src/main/java/to/bitkit/ui/screens/wallets/send/SendAmountScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/send/SendAmountScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -62,7 +63,7 @@ fun SendAmountScreen(
     onEvent: (SendEvent) -> Unit,
 ) {
     val currencyVM = currencyViewModel ?: return
-    var input: String by remember { mutableStateOf("") }
+    var input: String by remember { mutableStateOf(uiState.amountInput) }
     var overrideSats: Long? by remember { mutableStateOf(null) }
 
     AmountInputHandler(
