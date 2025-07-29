96fdb70afc21274fe86003c99f9e95aa98ff4e56
Joao Victor Sena <jvsena16@gmail.com>
2025-07-29 11:57:35 -0300

    feat: preview

--- app/src/main/java/to/bitkit/ui/screens/transfer/SpendingAmountScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/transfer/SpendingAmountScreen.kt
@@ -12,10 +12,10 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import to.bitkit.R
@@ -30,9 +30,12 @@ import to.bitkit.ui.components.UnitButton
 import to.bitkit.ui.scaffold.AppTopBar
 import to.bitkit.ui.scaffold.CloseNavIcon
 import to.bitkit.ui.scaffold.ScreenColumn
+import to.bitkit.ui.theme.AppThemeSurface
 import to.bitkit.ui.theme.Colors
 import to.bitkit.ui.utils.withAccent
+import to.bitkit.viewmodels.CurrencyUiState
 import to.bitkit.viewmodels.TransferEffect
+import to.bitkit.viewmodels.TransferToSpendingUiState
 import to.bitkit.viewmodels.TransferViewModel
 
 @Composable
@@ -61,6 +64,29 @@ fun SpendingAmountScreen(
         }
     }
 
+    Content(
+        uiState = uiState,
+        currencies = currencies,
+        onBackClick = onBackClick,
+        onCloseClick = onCloseClick,
+        onClickQuarter = viewModel::onClickQuarter,
+        onClickMaxAmount = viewModel::onClickMaxAmount,
+        onConfirmAmount = viewModel::onConfirmAmount,
+        onAmountChanged = viewModel::onAmountChanged
+    )
+}
+
+@Composable
+private fun Content(
+    uiState: TransferToSpendingUiState,
+    currencies: CurrencyUiState,
+    onBackClick: () -> Unit,
+    onCloseClick: () -> Unit,
+    onClickQuarter: () -> Unit,
+    onClickMaxAmount: () -> Unit,
+    onConfirmAmount: () -> Unit,
+    onAmountChanged: (Long) -> Unit,
+) {
     ScreenColumn {
         AppTopBar(
             titleText = stringResource(R.string.lightning__transfer__nav_title),
@@ -84,7 +110,7 @@ fun SpendingAmountScreen(
             AmountInput(
                 primaryDisplay = currencies.primaryDisplay,
                 overrideSats = uiState.overrideSats,
-                onSatsChange = viewModel::onAmountChanged,
+                onSatsChange = onAmountChanged,
             )
 
             Spacer(modifier = Modifier.weight(1f))
@@ -109,13 +135,13 @@ fun SpendingAmountScreen(
                 NumberPadActionButton(
                     text = stringResource(R.string.lightning__spending_amount__quarter),
                     color = Colors.Purple,
-                    onClick = viewModel::onClickQuarter,
+                    onClick = onClickQuarter,
                 )
                 // Max Button
                 NumberPadActionButton(
                     text = stringResource(R.string.common__max),
                     color = Colors.Purple,
-                    onClick = viewModel::onClickMaxAmount,
+                    onClick = onClickMaxAmount,
                 )
             }
             HorizontalDivider()
@@ -123,12 +149,29 @@ fun SpendingAmountScreen(
 
             PrimaryButton(
                 text = stringResource(R.string.common__continue),
-                onClick = viewModel::onConfirmAmount,
+                onClick = onConfirmAmount,
                 enabled = uiState.satsAmount != 0L && uiState.satsAmount <= uiState.maxAllowedToSend,
                 isLoading = uiState.isLoading,
             )
 
             Spacer(modifier = Modifier.height(16.dp))
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        Content(
+            uiState = TransferToSpendingUiState(),
+            currencies = CurrencyUiState(),
+            onBackClick = {},
+            onCloseClick = {},
+            onClickQuarter = {},
+            onClickMaxAmount = {},
+            onConfirmAmount = {},
+            onAmountChanged = {},
+        )
     }
 }
