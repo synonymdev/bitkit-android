13954ea81c34b0c191fab913d1ecd8b43a064b50
Joao Victor Sena <jvsena16@gmail.com>
2025-08-29 14:07:02 -0300

    feat: detail visibility

--- app/src/main/java/to/bitkit/ui/screens/wallets/send/SendErrorScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/send/SendErrorScreen.kt
@@ -1,18 +1,24 @@
 package to.bitkit.ui.screens.wallets.send
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -39,11 +45,17 @@ fun SendErrorScreen(
     onRetry: () -> Unit,
     onClose: () -> Unit,
 ) {
+    var displayDetails by remember { mutableStateOf(false) }
+
     Content(
         errorMessage = errorMessage,
         stackTrace = stackTrace,
         onRetry = onRetry,
         onClose = onClose,
+        displayDetails = displayDetails,
+        onDisplayDetails = {
+            displayDetails = true
+        }
     )
 }
 
@@ -51,7 +63,10 @@ fun SendErrorScreen(
 private fun Content(
     errorMessage: String?,
     stackTrace: String?,
+    displayDetails: Boolean,
     modifier: Modifier = Modifier,
+    onDisplayDetails: () -> Unit = {},
+    onSendReportClick: () -> Unit = {},
     onRetry: () -> Unit = {},
     onClose: () -> Unit = {},
 ) {
@@ -60,6 +75,7 @@ private fun Content(
         modifier = modifier
             .fillMaxSize()
             .gradientBackground()
+            .verticalScroll(rememberScrollState())
             .navigationBarsPadding()
     ) {
         SheetTopBar(stringResource(R.string.wallet__send_error_tx_failed))
@@ -85,15 +101,20 @@ private fun Content(
 
                 BodyM(text = errorText, color = Colors.White64)
 
-                BodyM(
-                    text = stackTrace.orEmpty(),
-                    color = Colors.Red,
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .fillMaxWidth()
-                        .background(color = Colors.White10, shape = Shapes.medium)
-                        .padding(16.dp)
-                )
+                AnimatedVisibility(
+                    visible = displayDetails && !stackTrace.isNullOrBlank(),
+                    label = ""
+                ) {
+                    BodyM(
+                        text = stackTrace.orEmpty(),
+                        color = Colors.Red,
+                        modifier = Modifier
+                            .padding(top = 16.dp)
+                            .fillMaxWidth()
+                            .background(color = Colors.White10, shape = Shapes.medium)
+                            .padding(16.dp)
+                    )
+                }
 
                 Spacer(modifier = Modifier.weight(1f))
 
@@ -101,23 +122,38 @@ private fun Content(
             }
         }
 
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
         ) {
-            SecondaryButton(
-                text = stringResource(R.string.common__cancel),
-                onClick = onClose,
-                fullWidth = false,
-                modifier = Modifier
-                    .weight(1f)
-                    .testTag("Close")
-            )
+            AnimatedVisibility(visible = !stackTrace.isNullOrBlank() && !displayDetails) {
+                SecondaryButton(
+                    text = "Technical Details", // TODO Transifex
+                    onClick = onDisplayDetails,
+                    modifier = Modifier
+                )
+            }
+            AnimatedVisibility(visible = !stackTrace.isNullOrBlank() && displayDetails) {
+                SecondaryButton(
+                    text = "Send Anonymous Report", // TODO Transifex
+                    onClick = onSendReportClick,
+                    modifier = Modifier
+                )
+            }
+            if (stackTrace.isNullOrBlank()) {
+                SecondaryButton(
+                    text = stringResource(R.string.common__cancel),
+                    onClick = onClose,
+                    fullWidth = true,
+                    modifier = Modifier
+                        .testTag("Close")
+                )
+            }
             PrimaryButton(
                 text = stringResource(R.string.common__try_again),
                 onClick = onRetry,
-                fullWidth = false,
-                modifier = Modifier.weight(1f)
             )
         }
 
@@ -135,8 +171,35 @@ private fun Preview() {
                 stackTrace = "Test render error\n" +
                     "\n" +
                     "     in Tabbar (at WalletNavigator.tsx:59)\n" +
+                    "     in Tabbar (at WalletNavigator.tsx:59)\n" +
+                    "     in Tabbar (at WalletNavigator.tsx:59)\n" +
+                    "     in Tabbar (at WalletNavigator.tsx:59)\n" +
                     "     in WalletsStack (at SceneView.tsx:132)",
                 modifier = Modifier.sheetHeight(),
+                displayDetails = true,
+                onDisplayDetails = {}
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun PreviewDontDisplayDetails() {
+    AppThemeSurface {
+        BottomSheetPreview {
+            Content(
+                errorMessage = stringResource(R.string.wallet__send_error_create_tx),
+                stackTrace = "Test render error\n" +
+                    "\n" +
+                    "     in Tabbar (at WalletNavigator.tsx:59)\n" +
+                    "     in Tabbar (at WalletNavigator.tsx:59)\n" +
+                    "     in Tabbar (at WalletNavigator.tsx:59)\n" +
+                    "     in Tabbar (at WalletNavigator.tsx:59)\n" +
+                    "     in WalletsStack (at SceneView.tsx:132)",
+                modifier = Modifier.sheetHeight(),
+                displayDetails = false,
+                onDisplayDetails = {}
             )
         }
     }
@@ -151,6 +214,8 @@ private fun PreviewUnknown() {
                 errorMessage = "",
                 stackTrace = null,
                 modifier = Modifier.sheetHeight(),
+                displayDetails = false,
+                onDisplayDetails = {}
             )
         }
     }
