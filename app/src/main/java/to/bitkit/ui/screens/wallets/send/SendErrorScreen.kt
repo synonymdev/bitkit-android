fd6b05ed768500d3fe23409acbf0af3053ec5c41
Joao Victor Sena <jvsena16@gmail.com>
2025-08-29 13:33:02 -0300

    feat: display detail box

--- app/src/main/java/to/bitkit/ui/screens/wallets/send/SendErrorScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/send/SendErrorScreen.kt
@@ -1,7 +1,9 @@
 package to.bitkit.ui.screens.wallets.send
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
@@ -27,6 +30,7 @@ import to.bitkit.ui.shared.modifiers.sheetHeight
 import to.bitkit.ui.shared.util.gradientBackground
 import to.bitkit.ui.theme.AppThemeSurface
 import to.bitkit.ui.theme.Colors
+import to.bitkit.ui.theme.Shapes
 
 @Composable
 fun SendErrorScreen(
@@ -60,45 +64,64 @@ private fun Content(
     ) {
         SheetTopBar(stringResource(R.string.wallet__send_error_tx_failed))
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 16.dp)
+        Box(
+            modifier = Modifier.weight(1f)
         ) {
-            Spacer(modifier = Modifier.height(16.dp))
-
-            BodyM(text = errorText, color = Colors.White64)
-
-            Spacer(modifier = Modifier.weight(1f))
             Image(
                 painter = painterResource(R.drawable.cross),
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(256.dp)
+                    .align(Alignment.Center)
             )
-            Spacer(modifier = Modifier.weight(1f))
 
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier.fillMaxWidth()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp)
             ) {
-                SecondaryButton(
-                    text = stringResource(R.string.common__cancel),
-                    onClick = onClose,
-                    modifier = Modifier
-                        .weight(1f)
-                        .testTag("Close")
-                )
-                PrimaryButton(
-                    text = stringResource(R.string.common__try_again),
-                    onClick = onRetry,
-                    modifier = Modifier.weight(1f)
-                )
-            }
+                Spacer(modifier = Modifier.height(16.dp))
 
-            Spacer(modifier = Modifier.height(16.dp))
+                BodyM(text = errorText, color = Colors.White64)
+
+                BodyM(
+                    text = stackTrace.orEmpty(),
+                    color = Colors.Red,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .fillMaxWidth()
+                        .background(color = Colors.White10, shape = Shapes.medium)
+                        .padding(16.dp)
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+
+            }
         }
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            SecondaryButton(
+                text = stringResource(R.string.common__cancel),
+                onClick = onClose,
+                fullWidth = false,
+                modifier = Modifier
+                    .weight(1f)
+                    .testTag("Close")
+            )
+            PrimaryButton(
+                text = stringResource(R.string.common__try_again),
+                onClick = onRetry,
+                fullWidth = false,
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
