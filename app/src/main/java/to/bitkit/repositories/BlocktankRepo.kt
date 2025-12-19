9dcf16fed68b744debaed027f4e0404b8f061bbb
jvsena42 <jvsena16@gmail.com>
2025-12-19 06:54:34 -0300

    fix: wait for node run before open order

--- app/src/main/java/to/bitkit/repositories/BlocktankRepo.kt
+++ app/src/main/java/to/bitkit/repositories/BlocktankRepo.kt
@@ -33,6 +33,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 import org.lightningdevkit.ldknode.ChannelDetails
 import to.bitkit.async.ServiceQueue
 import to.bitkit.data.CacheStore
@@ -52,6 +53,7 @@ import javax.inject.Named
 import javax.inject.Singleton
 import kotlin.math.ceil
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 @Singleton
@@ -282,6 +284,11 @@ class BlocktankRepo @Inject constructor(
     suspend fun openChannel(orderId: String): Result<IBtOrder> = withContext(bgDispatcher) {
         try {
             Logger.debug("Opening channel for order: '$orderId'", context = TAG)
+
+            withTimeout(1.minutes) {
+                lightningRepo.lightningState.first { it.nodeStatus?.isRunning ?: false }
+            }
+
             val order = coreService.blocktank.open(orderId)
 
             // Update the order in state
