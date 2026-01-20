c385d1b9adc813a01254958160df049504d47d81
jvsena42 <jvsena16@gmail.com>
2026-01-20 14:52:42 -0300

    chore: lint

--- app/src/main/java/to/bitkit/androidServices/LightningNodeService.kt
+++ app/src/main/java/to/bitkit/androidServices/LightningNodeService.kt
@@ -4,7 +4,6 @@ import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
-import android.content.pm.ServiceInfo
 import android.os.Build
 import android.os.IBinder
 import androidx.annotation.RequiresApi
