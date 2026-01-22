f4c5253b3f293c09639439e8142281cfdc8332d5
Ovi Trif <ovitrif@proton.me>
2026-01-22 02:53:34 +0100

    chore: lint

--- app/src/main/java/to/bitkit/ui/components/DrawerMenu.kt
+++ app/src/main/java/to/bitkit/ui/components/DrawerMenu.kt
@@ -33,8 +33,8 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
