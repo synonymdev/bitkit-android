756c6ef99ef64c349ccf6ade14cf4de35f288c92
Ovi Trif <ovitrif@proton.me>
2026-02-19 18:55:03 +0100

    fix: remove end slash from repo url

--- app/src/main/java/to/bitkit/env/Env.kt
+++ app/src/main/java/to/bitkit/env/Env.kt
@@ -128,7 +128,7 @@ internal object Env {
     const val APP_STORE_URL = "https://apps.apple.com/app/bitkit-wallet/id6502440655"
     const val PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=to.bitkit"
 
-    const val REPO_URL = "https://github.com/synonymdev/bitkit-android/"
+    const val REPO_URL = "https://github.com/synonymdev/bitkit-android"
     const val RELEASE_URL = "$REPO_URL/releases/updater/release.json"
     const val EXCHANGES_URL = "https://bitcoin.org/en/exchanges#international"
     const val BTC_MAP_URL = "https://btcmap.org/map"
