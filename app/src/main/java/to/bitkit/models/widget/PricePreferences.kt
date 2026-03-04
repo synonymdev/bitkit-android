5e3ff4b0072779e3d8d23f787566bf1f54e75a5d
jvsena42 <jvsena16@gmail.com>
2026-03-04 08:51:20 -0300

    chore: add immutability annotation

--- app/src/main/java/to/bitkit/models/widget/PricePreferences.kt
+++ app/src/main/java/to/bitkit/models/widget/PricePreferences.kt
@@ -1,12 +1,14 @@
 package to.bitkit.models.widget
 
+import androidx.compose.runtime.Stable
 import kotlinx.serialization.Serializable
 import to.bitkit.data.dto.price.GraphPeriod
 import to.bitkit.data.dto.price.TradingPair
 
+@Stable
 @Serializable
 data class PricePreferences(
-    val enabledPairs: List<TradingPair> = listOf(
+    @Stable val enabledPairs: List<TradingPair> = listOf(
         TradingPair.BTC_USD
     ),
     val period: GraphPeriod? = GraphPeriod.ONE_DAY,
