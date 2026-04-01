65b376d1c04d12756f68bca7e50b831f6c81664a
jvsena42 <jvsena16@gmail.com>
2026-04-01 07:28:15 -0300

    chore: lint

--- app/src/main/java/to/bitkit/ui/settings/pin/PinManagementScreen.kt
+++ app/src/main/java/to/bitkit/ui/settings/pin/PinManagementScreen.kt
@@ -11,9 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
