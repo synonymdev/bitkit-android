a6641f4ca3360240a53a11f9e35db6470f43d4f8
jvsena42 <jvsena16@gmail.com>
2026-04-02 13:19:29 -0300

    refactor: rename component and add preview

--- app/src/main/java/to/bitkit/ui/components/SendCell.kt
+++ app/src/main/java/to/bitkit/ui/components/SendCell.kt
@@ -3,13 +3,16 @@ package to.bitkit.ui.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import to.bitkit.ui.theme.AppThemeSurface
 import to.bitkit.ui.theme.Colors
 
 @Composable
-fun SendSectionView(
+fun SendCell(
     caption: String,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
@@ -20,5 +23,15 @@ fun SendSectionView(
         content()
         VerticalSpacer(16.dp)
         HorizontalDivider(modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun Preview() {
+    AppThemeSurface {
+        SendCell(caption = "AMOUNT") {
+            Text("0.001 BTC", color = Colors.White)
+        }
     }
 }
