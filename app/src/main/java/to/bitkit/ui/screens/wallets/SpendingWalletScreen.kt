2d9d0d358402818328dfb61858d8045fc046e196
Ovi Trif <ovitrif@proton.me>
2026-04-06 20:39:45 +0200

    chore: rm unused import

--- app/src/main/java/to/bitkit/ui/screens/wallets/SpendingWalletScreen.kt
+++ app/src/main/java/to/bitkit/ui/screens/wallets/SpendingWalletScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
