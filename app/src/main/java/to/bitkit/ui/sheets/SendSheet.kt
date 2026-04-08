0a02b8992afe1d0c1283c67a66a96f34e7822b48
jvsena42 <jvsena16@gmail.com>
2026-04-08 10:38:34 -0300

    chore: lint

--- app/src/main/java/to/bitkit/ui/sheets/SendSheet.kt
+++ app/src/main/java/to/bitkit/ui/sheets/SendSheet.kt
@@ -11,8 +11,8 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
