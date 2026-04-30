eee104b65e9ab08daa87486f9c84401e94024a1a
jvsena42 <jvsena16@gmail.com>
2026-04-30 14:39:26 -0300

    fix: ui tests

--- app/src/androidTest/java/to/bitkit/ui/screens/widgets/headlines/HeadlinesPreviewContentTest.kt
+++ app/src/androidTest/java/to/bitkit/ui/screens/widgets/headlines/HeadlinesPreviewContentTest.kt
@@ -51,18 +51,12 @@ class HeadlinesPreviewContentTest {
 
         // Assert main elements exist
         composeTestRule.onNodeWithTag("headlines_preview_screen").assertExists()
-        composeTestRule.onNodeWithTag("main_content").assertExists()
-
-        // Verify header elements
-        composeTestRule.onNodeWithTag("header_row").assertExists()
-        composeTestRule.onNodeWithTag("widget_title").assertExists()
-        composeTestRule.onNodeWithTag("widget_icon").assertExists()
         composeTestRule.onNodeWithTag("widget_description").assertExists()
 
         // Verify settings and preview section
         composeTestRule.onNodeWithTag("WidgetEdit").assertExists()
-        composeTestRule.onNodeWithTag("preview_label").assertExists()
-        composeTestRule.onNodeWithTag("headline_card").assertExists()
+        composeTestRule.onNodeWithTag("headlines_preview_carousel").assertExists()
+        composeTestRule.onNodeWithTag("headline_card_small").assertExists()
 
         // Verify buttons
         composeTestRule.onNodeWithTag("buttons_row").assertExists()
@@ -142,7 +136,7 @@ class HeadlinesPreviewContentTest {
         // Assert that all elements still exist with custom preferences
         composeTestRule.onNodeWithTag("headlines_preview_screen").assertExists()
         composeTestRule.onNodeWithTag("WidgetEdit").assertExists()
-        composeTestRule.onNodeWithTag("headline_card").assertExists()
+        composeTestRule.onNodeWithTag("headlines_preview_carousel").assertExists()
     }
 
     @Test
@@ -164,15 +158,11 @@ class HeadlinesPreviewContentTest {
 
         // Assert all tagged elements exist
         composeTestRule.onNodeWithTag("headlines_preview_screen").assertExists()
-        composeTestRule.onNodeWithTag("main_content").assertExists()
-        composeTestRule.onNodeWithTag("header_row").assertExists()
-        composeTestRule.onNodeWithTag("widget_title").assertExists()
-        composeTestRule.onNodeWithTag("widget_icon").assertExists()
         composeTestRule.onNodeWithTag("widget_description").assertExists()
         composeTestRule.onNodeWithTag("divider").assertExists()
         composeTestRule.onNodeWithTag("WidgetEdit").assertExists()
-        composeTestRule.onNodeWithTag("preview_label").assertExists()
-        composeTestRule.onNodeWithTag("headline_card").assertExists()
+        composeTestRule.onNodeWithTag("headlines_preview_carousel").assertExists()
+        composeTestRule.onNodeWithTag("headline_card_small").assertExists()
         composeTestRule.onNodeWithTag("buttons_row").assertExists()
         composeTestRule.onNodeWithTag("WidgetDelete").assertExists()
         composeTestRule.onNodeWithTag("WidgetSave").assertExists()
@@ -223,7 +213,7 @@ class HeadlinesPreviewContentTest {
 
         // Assert core elements still exist
         composeTestRule.onNodeWithTag("headlines_preview_screen").assertExists()
-        composeTestRule.onNodeWithTag("headline_card").assertExists()
+        composeTestRule.onNodeWithTag("headlines_preview_carousel").assertExists()
         composeTestRule.onNodeWithTag("WidgetSave").assertExists()
         composeTestRule.onNodeWithTag("WidgetDelete").assertDoesNotExist()
     }
