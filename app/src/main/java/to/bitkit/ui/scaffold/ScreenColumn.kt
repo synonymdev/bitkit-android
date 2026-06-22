10a632752b9d309ef38f2f19cdc09156cc45b5dc
Ovi Trif <ovitrif@proton.me>
2026-06-22 18:33:37 +0200

    fix: clip screen bounds

--- app/src/main/java/to/bitkit/ui/scaffold/ScreenColumn.kt
+++ app/src/main/java/to/bitkit/ui/scaffold/ScreenColumn.kt
@@ -1,15 +1,12 @@
 package to.bitkit.ui.scaffold
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import to.bitkit.ui.shared.util.screen
 
 @Composable
 fun ScreenColumn(
@@ -17,6 +14,7 @@ fun ScreenColumn(
     verticalArrangement: Arrangement.Vertical = Arrangement.Top,
     horizontalAlignment: Alignment.Horizontal = Alignment.Start,
     noBackground: Boolean = false,
+    clipToBounds: Boolean = true,
     content: @Composable ColumnScope.() -> Unit,
 ) {
     Column(
@@ -24,9 +22,10 @@ fun ScreenColumn(
         horizontalAlignment = horizontalAlignment,
         content = content,
         modifier = Modifier
-            .fillMaxSize()
-            .then(if (noBackground) Modifier else Modifier.background(MaterialTheme.colorScheme.background))
-            .systemBarsPadding()
+            .screen(
+                noBackground = noBackground,
+                clipToBounds = clipToBounds,
+            )
             .then(modifier)
     )
 }
